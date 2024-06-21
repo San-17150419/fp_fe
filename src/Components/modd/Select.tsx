5aef19fb16750941015932c1baa6d2d37c0b27f1
ZENG-JING-YUAN <steven.tseng@heavypower.com.tw>
2024-06-21 17:29:15 +0800

    refactor: improve Select component styling
    - Update Select component to use a div wrapper with improved styling.
    - Clean up the code for better readability and maintenance.

--- src/Components/modd/Select.tsx
+++ src/Components/modd/Select.tsx
@@ -1,17 +1,24 @@
-import {type SelectHTMLAttributes } from 'react'
+import { type SelectHTMLAttributes } from "react";
 
-interface SelectProps extends SelectHTMLAttributes < HTMLSelectElement > {
-    options: string[]
+interface SelectProps extends SelectHTMLAttributes<HTMLSelectElement> {
+  options: string[];
 }
 
-
 export default function Select({ options, ...props }: SelectProps) {
-    return (
-        // TODO: Extract width to a variable
-        <span style={{ width: "180px" }}>
-            <select className="form-select fw-bolder" {...props} >
-                {options.map((option, index) => <option className="border-start border-3 border-black fw-bolder" key={index} value={option}>{option}</option>)}
-            </select>
-        </span>
-    )
+  return (
+    // TODO: Extract width to a variable
+    <div className="px-2 m-1 flex h-[38px] w-[180px] rounded-md border border-gray-300">
+      <select className="w-full" {...props}>
+        {options.map((option, index) => (
+          <option
+            className="border-start border-3 fw-bolder border-black"
+            key={index}
+            value={option}
+          >
+            {option}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
 }
