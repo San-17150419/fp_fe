b9a0a5805e0e97f1a923ee3758908e0c80f5df30
ZENG-JING-YUAN <steven.tseng@heavypower.com.tw>
2024-06-21 17:25:50 +0800

    refactor: enhance SelectTestVersion component
    - Refactor SelectTestVersion component to accept dynamic options.
    - Update the component to use the Listbox from @headlessui/react for
    better accessibility and UI.
    - Improve the focus and active state styles for better user experience.
    - Clean up and organize imports.

--- src/SelectTestVersion.tsx
+++ src/SelectTestVersion.tsx
@@ -1,39 +1,80 @@
-import { Select } from '@headlessui/react';
-import { Menu, Transition, MenuButton, MenuItem, MenuItems } from '@headlessui/react';
 import { PiCaretDownBold } from "react-icons/pi";
-import { Listbox, ListboxButton, ListboxOption, ListboxOptions } from '@headlessui/react'
-import { useState } from 'react'
+import {
+  Listbox,
+  ListboxButton,
+  ListboxOption,
+  ListboxOptions,
+  Transition,
+} from "@headlessui/react";
+import { useState } from "react";
 import { RxCaretDown } from "react-icons/rx";
 
-const people = [
-    { id: 1, name: 'Durward Reynolds' },
-    { id: 2, name: 'Kenton Towne' },
-    { id: 3, name: 'Therese Wunsch' },
-    { id: 4, name: 'Benedict Kessler' },
-    { id: 5, name: 'Katelyn Rohan' },
-]
-export default function SelectTestVersion() {
-    const [selectedPerson, setSelectedPerson] = useState(people[0])
-    return (
-        // <Select name="status" className="border border-gray-300 min-w-[180px] rounded" aria-label="Project status">
-        //     <option value="active">Active</option>
-        //     <option value="paused">Paused</option>
-        //     <option value="delayed">Delayed</option>
-        //     <option value="canceled">Canceled</option>
-        // </Select>
+// options is an array of strings or an array of objects. If it's an array of objects, the id property is required. The value property is required.
+// I am not sure what benifits it has. It might be useful in the future. If not, I can remove it. It might make more sense to just pass an array of strings. You can easily extract certain property from the object and make it an array of strings.
+type Options =
+  | string[]
+  | Array<{ id: string | number; value: string | number }>;
 
-        <Listbox value={selectedPerson} onChange={setSelectedPerson} >
-            <ListboxButton className="border border-gray-300 min-w-[180px] rounded flex gap-2 justify-center items-center font-bold px-2">
-                資料庫檢索及更新
-                <PiCaretDownBold className='font-bold' />
-            </ListboxButton>
-            <ListboxOptions anchor="bottom" className={"z-40"}>
-                {people.map((person) => (
-                    <ListboxOption key={person.id} value={person} className="data-[focus]:bg-blue-100 z-50">
-                        {person.name}
-                    </ListboxOption>
-                ))}
+type SelectProps = {
+  options: Options;
+};
+
+export default function SelectTestVersion({ options }: SelectProps) {
+  const [selectedOption, setSelectedOption] = useState(options[0]);
+
+  return (
+    <Listbox
+      value={selectedOption}
+      onChange={setSelectedOption}
+      as={"div"}
+      className={"m-1"}
+    >
+      {({ open }) => (
+        <>
+          <ListboxButton
+            data-open={open}
+            className={`mb-1 flex min-w-[180px] items-center rounded border border-gray-300 py-1 font-bold focus-visible:outline-none focus-visible:ring-2 data-[focus]:border-gray-300 data-[active]:ring-2 data-[focus]:ring-2`}
+            //
+          >
+            <span className="mx-1">
+              {typeof selectedOption === "object"
+                ? selectedOption.value
+                : selectedOption}
+            </span>
+            <PiCaretDownBold className="ml-auto mr-2 font-bold transition-all" />
+          </ListboxButton>
+          <Transition
+            enter="transition ease-out duration-200"
+            enterFrom="transform opacity-0 scale-95"
+            enterTo="transform opacity-100 scale-100"
+            leave="transition ease-in duration-100"
+            leaveFrom="transform opacity-100 scale-100"
+            leaveTo="transform opacity-0 scale-95"
+          >
+            <ListboxOptions
+              anchor="bottom"
+              //To match the width of the dropdown with the width of the button, use the --button-width CSS variable that's exposed on the ListboxOptions element
+              className={
+                "z-10 w-[var(--button-width)] rounded-md border border-gray-300 bg-white [--anchor-gap:2px]"
+              }
+            >
+              {options.map((option) => (
+                <ListboxOption
+                  key={
+                    typeof selectedOption === "object"
+                      ? selectedOption.id
+                      : selectedOption
+                  }
+                  value={option}
+                  className={`${option === selectedOption ? "border-zinc-500" : "border-transparent"} z-20 cursor-pointer p-2 hover:bg-gray-400 data-[selected]:border-l-2 data-[active]:bg-gray-400`}
+                >
+                  {typeof option === "object" ? option.value : option}
+                </ListboxOption>
+              ))}
             </ListboxOptions>
-        </Listbox>
-    )
+          </Transition>
+        </>
+      )}
+    </Listbox>
+  );
 }
