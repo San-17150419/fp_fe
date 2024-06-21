4ea7faa191df7c8edfa205f538c10795f7f4657f
ZENG-JING-YUAN <steven.tseng@heavypower.com.tw>
2024-06-21 17:28:14 +0800

    refactor: enhance Sidebar component with NavLink
    - Refactor Sidebar component to use NavLink for better routing and
    active state management.
    - Replace static menu items with a dynamic list of options.
    - Improve the visual feedback for menu items and transition animations.
    - Clean up and organize the code for better readability and
    maintainability.

--- src/Components/modd/Sidebar.tsx
+++ src/Components/modd/Sidebar.tsx
@@ -1,60 +1,79 @@
-import React, { useState, useEffect } from 'react';
-import { Menu, MenuButton, MenuItems, MenuItem, Transition } from '@headlessui/react';
-import { IoCaretDownSharp } from 'react-icons/io5';
+import { useState, useEffect } from "react";
+import {
+  Menu,
+  MenuButton,
+  MenuItems,
+  MenuItem,
+  Transition,
+} from "@headlessui/react";
+import { IoCaretDownSharp } from "react-icons/io5";
+import { NavLink } from "react-router-dom";
+// TODO: Use Listbox instead of MenuItems.
 
 export default function Sidebar() {
-    const [isOpen, setIsOpen] = useState(true);
+  const [isOpen, setIsOpen] = useState(true);
+  const options = [
+    { id: 1, value: "Home Page", path: "/" },
+    { id: 2, value: "Input", path: "/input" },
+    { id: 3, value: "Select Page", path: "/select" },
+    { id: 4, value: "Data Table", path: "/table" },
+  ];
 
-    // Automatically open the menu when the component mounts
-    useEffect(() => {
-        setIsOpen(true);
-    }, []);
+  // Automatically open the menu when the component mounts
+  useEffect(() => {
+    setIsOpen(true);
+  }, []);
 
-    return (
-        <aside className='h-screen w-4/6 border border-black p-4'>
-            <Menu as="div" className="relative inline-block text-left" >
-                <div>
-                    <MenuButton onClick={() => setIsOpen(!isOpen)} className="flex items-center gap-4 font-semibold hover:font-bold hover:text-blue-500">
-                        <IoCaretDownSharp size={21} />
-                        元件展示
-                    </MenuButton>
-                </div>
-                <Transition
-                    show={isOpen}
-                    enter="transition ease-out duration-200"
-                    enterFrom="transform opacity-0 scale-95"
-                    enterTo="transform opacity-100 scale-100"
-                    leave="transition ease-in duration-100"
-                    leaveFrom="transform opacity-100 scale-100"
-                    leaveTo="transform opacity-0 scale-95"
-                >
-                    <MenuItems className="origin-top-right absolute left-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
-                        <div className="py-1">
-                            <MenuItem>
-                                {({ focus }) => (
-                                    <a className={`block px-4 py-2 text-sm text-gray-700 ${focus ? 'bg-gray-100' : ''}`} href="/input">
-                                        Input Element
-                                    </a>
-                                )}
-                            </MenuItem>
-                            <MenuItem>
-                                {({ focus }) => (
-                                    <a className={`block px-4 py-2 text-sm text-gray-700 ${focus ? 'bg-gray-100' : ''}`} href="/select">
-                                        Select Element
-                                    </a>
-                                )}
-                            </MenuItem>
-                            <MenuItem>
-                                {({ focus }) => (
-                                    <a className={`block px-4 py-2 text-sm text-gray-700 ${focus ? 'bg-gray-100' : ''}`} href="/dataTable">
-                                        Data Table
-                                    </a>
-                                )}
-                            </MenuItem>
-                        </div>
-                    </MenuItems>
-                </Transition>
-            </Menu>
-        </aside>
-    );
+  return (
+    <aside className="z-20 h-screen border border-black p-4">
+      <Menu as="div" className="relative inline-block text-left">
+        <MenuButton
+          onClick={() => setIsOpen(!isOpen)}
+          className="flex items-center gap-2 font-semibold hover:font-bold hover:text-blue-500"
+        >
+          {({ open }) => (
+            <>
+              <IoCaretDownSharp
+                size={21}
+                className={`${open && "-rotate-90"} transition-all duration-300`}
+              />
+              元件展示
+            </>
+          )}
+        </MenuButton>
+        <Transition
+          show={isOpen}
+          enter="transition ease-out duration-200"
+          enterFrom="transform opacity-0 scale-95"
+          enterTo="transform opacity-100 scale-100"
+          leave="transition ease-in duration-100"
+          leaveFrom="transform opacity-100 scale-100"
+          leaveTo="transform opacity-0 scale-95"
+        >
+          <MenuItems className="text-decoration-none absolute left-6 z-40 mt-2 w-32 origin-top-right bg-white">
+            <div className="py-1">
+              {options.map((option) => (
+                <MenuItem key={option.id}>
+                  {({ focus }) => (
+                    <NavLink
+                      to={option.path}
+                      className={({ isActive }) =>
+                        `${
+                          isActive ? "bg-gray-300" : ""
+                        } text-decoration-none mb-1 block rounded-full px-4 py-2 text-sm font-semibold text-gray-700 hover:bg-gray-200 ${
+                          focus ? "bg-gray-100" : ""
+                        }`
+                      }
+                    >
+                      {option.value}
+                    </NavLink>
+                  )}
+                </MenuItem>
+              ))}
+            </div>
+          </MenuItems>
+        </Transition>
+      </Menu>
+    </aside>
+  );
 }
