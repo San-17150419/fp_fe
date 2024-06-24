dddded28917ba455a83693b72196148d25a98f7f
ZENG-JING-YUAN <steven.tseng@heavypower.com.tw>
2024-06-24 17:12:49 +0800

    feat: enhance Navbar component
    - Introduced new subcomponents for better structure and flexibility.
    - Added NavbarLogo, NavbarItems, and NavbarItem subcomponents with
    corresponding type definitions and props.

--- src/Components/modd/Navbar.tsx
+++ src/Components/modd/Navbar.tsx
@@ -1,7 +1,56 @@
-import React from 'react'
+import React, {
+  type AnchorHTMLAttributes,
+  type LiHTMLAttributes,
+  type ImgHTMLAttributes,
+  useEffect,
+} from "react";
 
-export default function Navbar() {
+import { FiMenu } from "react-icons/fi";
+
+type NavbarProps = {
+  children?: React.ReactNode;
+};
+export default function Navbar({ children }: NavbarProps) {
   return (
-    <div>Navbar</div>
-  )
+    <nav className="nav-custom flex min-h-20 items-center justify-start gap-8 px-4 py-2 shadow-2xl shadow-black">
+      {children}
+    </nav>
+  );
 }
+
+type NavbarLogoProps = ImgHTMLAttributes<HTMLImageElement>;
+
+function NavbarLogo({ src }: NavbarLogoProps) {
+  return <img className="w-48 object-contain" src={src} alt="" />;
+}
+
+type NavbarItemsProps = LiHTMLAttributes<HTMLUListElement> & {
+  children?: React.ReactNode;
+};
+function NavbarItems({ className = "", children }: NavbarItemsProps) {
+  return <ul className={"flex h-full gap-8" + className}>{children}</ul>;
+}
+
+type NavbarItemProps = AnchorHTMLAttributes<HTMLAnchorElement> & {
+  text?: string;
+  children?: React.ReactNode;
+};
+
+function NavbarItem({
+  children,
+  className = "",
+  text,
+  ...props
+}: NavbarItemProps) {
+  return (
+    <li className="mt-auto mb-4">
+      <a className={"font-bold text-white " + className} href="" {...props}>
+        {text || children}
+      </a>
+    </li>
+  );
+}
+
+Navbar.Logo = NavbarLogo;
+Navbar.Items = NavbarItems;
+Navbar.Item = NavbarItem;
