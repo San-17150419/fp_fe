3980f2fbcfc7e16a9557424d710c789797ee8dc6
ZENG-JING-YUAN <steven.tseng@heavypower.com.tw>
2024-07-03 15:49:22 +0800

    feat: Update Tailwind CSS configuration
    - Added custom screen size breakpoints to `tailwind.config.js` for
    better responsive design control:
      - `sm`: 640px
      - `tabletL`: 768px
      - `tabletP`: 800px
      - `lg`: 1024px
      - `desktop`: 1280px
      - `xl`: 1536

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    screens: {
+      sm: "640px",
+      tabletL: "768px",
+      tabletP: "800px",
+      lg: "1024px",
+      desktop: "1280px",
+      xl: "1536px",
+    },
     extend: {},
   },
   plugins: [],
-}
+};
