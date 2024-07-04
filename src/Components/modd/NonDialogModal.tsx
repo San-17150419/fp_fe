c227c1957b569db72a60c836e73c581a8473c82f
ZENG-JING-YUAN <steven.tseng@heavypower.com.tw>
2024-07-04 15:52:42 +0800

    feat: Enhance Modal functionality
    - Integrated framer-motion for modal entrance and exit animations.
    - Introduced two animation variants (leftTop and leftMiddle) for modal
     appearance.
    - Removed unused imports and replaced custom close and escape logic with the new hook.
    - Added now custom hook `useCloseAndEscape`:
        - Close the modal when clicking outside or pressing the Escape key.

--- src/Components/modd/NonDialogModal.tsx
+++ src/Components/modd/NonDialogModal.tsx
@@ -1,44 +1,76 @@
-import React, { useEffect, useRef } from "react";
-import Input from "./Input";
-import Button from "./Button";
-import MessageBox from "./MessageBox";
+import React, { useRef } from "react";
+import useCloseAndEscape from "../../hooks/useCloseAndEscape";
+import { AnimatePresence, motion } from "framer-motion";
 type ModalProps = {
   children?: React.ReactNode;
   onClose: () => void;
   openModal: boolean;
-  title?: string;
+  enter?: "leftTop" | "leftMiddle";
 };
 
-export default function Modal({ children, onClose, title }: ModalProps) {
+const leftTop = {
+  initial: { x: "-100vw", y: "-100vh", scale: 0.2 },
+  animate: { x: "-50%", y: "-50%", scale: 1 },
+  exit: { x: "-100vw", y: "-100vh", scale: 0.2 },
+  transition: { ease: "easeOut", duration: 0.8 },
+};
+
+const leftMiddle = {
+  initial: { x: "-100vw", y: "-50%", scale: 0.2 },
+  animate: { x: "-50%", y: "-50%", scale: 1 },
+  exit: { x: "-100vw", y: "-50%", scale: 0.2 },
+  transition: { ease: "easeOut", duration: 0.8 },
+};
+
+export default function Modal({
+  children,
+  onClose,
+  enter = "leftTop",
+  openModal,
+}: ModalProps) {
+  const modalRef = useRef<HTMLDivElement>(null);
+
+  // useClose(modalRef, onClose);
+  // useEscape(modalRef, onClose);
+  useCloseAndEscape(modalRef, onClose);
   return (
-    <div className="fixed bottom-0 left-0 right-0 top-0 z-50 flex items-center justify-center bg-black/30">
-      <div className="z-50 m-2 flex min-h-[500px] min-w-[500px] flex-col rounded-md bg-white">
-        <header className="relative m-2 flex min-h-12 items-center justify-center">
-          <h1 className="flex-grow text-2xl font-bold">{title}</h1>
-          <button
-            className="absolute right-2 h-8 w-8 rounded-full hover:bg-gray-300"
-            onClick={onClose}
+    <>
+      <AnimatePresence>
+        {openModal && (
+          <motion.div
+            className="fixed bottom-0 left-0 right-0 top-0 z-50 flex bg-black/50"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ ease: "easeOut", duration: 0.8 }}
           >
-            X
-          </button>
-        </header>
-        {/* TODO: */}
-        <div className="flex-grow border-y-2 border-gray-300">
-          <form id="myForm" className="m-4 flex" action="">
-            {children}
-          </form>
-        </div>
-        <div className="m-2 flex min-h-12 justify-end px-1 py-2">
-          <Button mode="cancel" onClick={onClose}>
-            Cancel
-          </Button>
-          <Button mode="submit" form="myForm" type="submit">
-            {/* Use form attribute to link the button to the form */}
-            Submit
-          </Button>
-          <Button>Default</Button>
-        </div>
-      </div>
-    </div>
+            <motion.div
+              initial={
+                enter === "leftTop" ? leftTop.initial : leftMiddle.initial
+              }
+              animate={
+                enter === "leftTop" ? leftTop.animate : leftMiddle.animate
+              }
+              exit={enter === "leftTop" ? leftTop.exit : leftMiddle.exit}
+              transition={
+                enter === "leftTop" ? leftTop.transition : leftMiddle.transition
+              }
+              className="relative left-1/2 top-1/2 aspect-square h-3/5 rounded-md bg-white px-4 py-6"
+              ref={modalRef}
+            >
+              <button
+                onClick={onClose}
+                className="absolute right-0 top-0 h-8 w-8 rounded-full hover:bg-gray-300"
+              >
+                X
+              </button>
+              <div className="h-full rounded-md border hover:border-black">
+                {children}
+              </div>
+            </motion.div>
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </>
   );
 }
