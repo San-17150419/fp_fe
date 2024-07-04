255141e8423f718a4cd25084004649e01b0bd18c
ZENG-JING-YUAN <steven.tseng@heavypower.com.tw>
2024-07-04 16:33:49 +0800

    refactor: Enhance Sidebar component
    - Consolidated SidebarTest functionality into the main Sidebar component.
    - Implemented an effect to close the sidebar when clicking outside of
    it.
    - Added another effect to highlight the last visited link using the
    useLocation hook.
    Incorporated GoSidebarCollapse icon for toggling sidebar visibility.
    Refactored options data import from SidebarLinkData.

--- src/Components/modd/Sidebar/Sidebar.tsx
+++ src/Components/modd/Sidebar/Sidebar.tsx
@@ -1,96 +1,80 @@
 /** @jsxImportSource @emotion/react */
+import { useState, Fragment, useRef, useEffect } from "react";
 import tw from "twin.macro";
-import { useState } from "react";
-import {
-  Menu,
-  MenuButton,
-  MenuItems,
-  MenuItem,
-  Transition,
-} from "@headlessui/react";
-import { IoCaretDownSharp } from "react-icons/io5";
-import { NavLink } from "react-router-dom";
+import { Transition } from "@headlessui/react";
+import { GoSidebarCollapse } from "react-icons/go";
+import { NavLink, useLocation } from "react-router-dom";
+import options from "../../../util/SidebarLinkData";
 
 export default function Sidebar() {
-  const [isOpen, setIsOpen] = useState(true);
-  const options = [
-    { id: 1, value: "Home Page", path: "/" },
-    { id: 2, value: "Input", path: "/input" },
-    { id: 3, value: "Select Page", path: "/select" },
-    {
-      id: 4,
-      value: "Data Table with non-dialog modal",
-      path: "/table-non-dialog",
-    },
-    // { id: 5, value: "Data Table with dialog modal", path: "/table-dialog" },
-    { id: 6, value: "Data Table with BFF pattern", path: "/bff" },
-    {
-      id: 7,
-      value: "Test Page for highcharts type 1",
-      path: "/highchart-type-1",
-    },
-    {
-      id: 8,
-      value: "Test Page for highcharts type 2",
-      path: "/highchart-type-2",
-    },
-    {
-      id: 9,
-      value: "Test Page for highcharts type 3",
-      path: "/highchart-type-3",
-    },
-    { id: 10, value: "Button Page", path: "/button" },
-    { id: 11, value: "HighCharts Page", path: "/highcharts" },
-  ];
+  const sidebarRef = useRef<HTMLDivElement | null>(null);
+
+  const [isOpen, setIsOpen] = useState(false);
+  const [lastVisited, setLastVisited] = useState<string | null>(null);
+  const location = useLocation();
+
+  useEffect(() => {
+    function handleClickOutside(event: MouseEvent) {
+      if (
+        sidebarRef.current &&
+        !sidebarRef.current.contains(event.target as Node)
+      ) {
+        setIsOpen(false);
+      }
+    }
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [sidebarRef]);
+
+  useEffect(() => {
+    setLastVisited(location.pathname);
+  }, [location]);
 
   return (
-    <aside tw="z-20 h-full border-e border-gray-300 bg-white p-4 shadow-xl">
-      <Menu as="div" tw="relative text-left">
-        <MenuButton
-          onClick={() => setIsOpen(!isOpen)}
-          tw="flex items-center gap-2 font-semibold hover:font-bold hover:text-blue-500"
+    <div className="relative flex w-10 bg-white px-2 pt-4" ref={sidebarRef}>
+      <GoSidebarCollapse
+        size={24}
+        css={[
+          tw`cursor-pointer mx-auto ml-auto transition-all duration-300 mt-2`,
+          !isOpen && tw`rotate-180 `,
+          isOpen && tw`rotate-0 `,
+        ]}
+        onClick={() => setIsOpen(!isOpen)}
+      />
+      <Transition
+        show={isOpen}
+        enter="transition-all ease-out duration-500"
+        enterFrom="transform opacity-0 "
+        enterTo="transform opacity-100 scale-100"
+        leave="transition ease-in duration-100"
+        leaveFrom="transform opacity-100 scale-100"
+        leaveTo="transform opacity-0 scale-95"
+        as={Fragment}
+      >
+        <div
+          css={[
+            tw`absolute left-10 px-4 py-6 top-0 z-50 h-full transition-all duration-300 min-w-max max-w-80 bg-white `,
+            !isOpen && tw`w-0 invisible `,
+          ]}
         >
-          {({ open }) => (
-            <>
-              <IoCaretDownSharp
-                size={21}
-                className={`${open && "-rotate-90"} transition-all duration-300`}
-              />
-              元件展示
-            </>
-          )}
-        </MenuButton>
-        <Transition
-          show={isOpen}
-          enter="transition ease-out duration-200"
-          enterFrom="transform opacity-0 scale-95"
-          enterTo="transform opacity-100 scale-100"
-          leave="transition ease-in duration-100"
-          leaveFrom="transform opacity-100 scale-100"
-          leaveTo="transform opacity-0 scale-95"
-          as={"div"}
-        >
-          <MenuItems tw=" z-40 mt-2 w-full origin-top-right bg-transparent">
-            <div tw="mx-6 py-1">
-              {options.map((option) => (
-                <MenuItem key={option.id} as="div">
-                  {({ focus }) => (
-                    <NavLink
-                      to={option.path}
-                      css={[
-                        tw` mb-1 block rounded-full px-4 py-2 text-sm font-semibold text-gray-700 hover:bg-gray-200`,
-                        focus && tw`bg-gray-100`,
-                      ]}
-                    >
-                      {option.value}
-                    </NavLink>
-                  )}
-                </MenuItem>
-              ))}
-            </div>
-          </MenuItems>
-        </Transition>
-      </Menu>
-    </aside>
+          {options.map((option) => (
+            <NavLink
+              key={option.text}
+              to={option.path}
+              css={[
+                tw` mb-1 block rounded-full px-4 py-2 text-xs font-semibold text-gray-700 hover:bg-gray-200`,
+                lastVisited === option.path && tw`bg-yellow-200`, // Highlight last visited link
+              ]}
+              onClick={() => setLastVisited(option.path)}
+            >
+              {option.text}
+            </NavLink>
+          ))}
+        </div>
+      </Transition>
+    </div>
   );
 }
