d3544c7c3ad6b8cf5d41228b27b2f5a47a494743
ZENG-JING-YUAN <steven.tseng@heavypower.com.tw>
2024-07-08 08:50:28 +0800

    refactor: Use reusable Loading component
    - Replaced inline loading logic in the Loading page with the reusable
    Loading component from the Components directory.

--- src/pages/Loading.tsx
+++ src/pages/Loading.tsx
@@ -1,10 +1,4 @@
-import { VscLoading } from 'react-icons/vsc';
-export default function Loading() {
-  return (
-    <div className="flex h-screen flex-col items-center justify-center gap-5 text-3xl">
-      <p className="loading">Loading ...</p>
-
-      <VscLoading className="animate-spin text-5xl" />
-    </div>
-  );
+import Loading from "../Components/Loading";
+export default function LoadingPage() {
+  return <Loading />;
 }
