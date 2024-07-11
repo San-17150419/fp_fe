af4eb1e40ad1eec16fa727dd2342c02713b6d0b0
ZENG-JING-YUAN <steven.tseng@heavypower.com.tw>
2024-07-11 16:52:35 +0800

    feat: add translation support and refactor Select
    - Enhanced the Select component to support translations using the
    i18next library.
    - Added a useEffect hook to handle initial selection.
    - Wrapped the Listbox in a Field component for better accessibility and
    structure.
    - Improved styling to reflect active and hover states accurately,
    ensuring a more user-friendly interface."

--- src/Components/modd/Select/Select.tsx
+++ src/Components/modd/Select/Select.tsx
@@ -3,11 +3,13 @@ import {
   ListboxButton,
   ListboxOption,
   ListboxOptions,
+  Field,
 } from "@headlessui/react";
 import { PiCaretDownBold } from "react-icons/pi";
 import { useSelect } from "./useSelect";
 import { SelectOption, DetailOption } from "./selectType";
-import "twin.macro";
+import { useEffect, useCallback } from "react";
+import { useTranslation } from "react-i18next";
 
 type SelectProps = {
   options: SelectOption[];
@@ -22,43 +24,59 @@ export default function Select({
   onSelect,
   name,
 }: SelectProps) {
+  const { t } = useTranslation();
   const { allOptions, selectedOption, setSelectedOption } = useSelect(options);
+  useEffect(() => {
+    if (onSelect) {
+      onSelect(selectedOption.value as string);
+    }
+  }, []);
 
   const handleSelect = (option: DetailOption) => {
     setSelectedOption(option);
     if (onSelect) {
-      onSelect(selectedOption.value as string);
+      onSelect(option.value as string);
     }
   };
 
   return (
-    <Listbox
-      value={selectedOption}
-      onChange={handleSelect}
-      name={name}
-      defaultValue={selectedOption}
-      as="div"
-      className="mx-1 my-5 flex h-[38px] w-full pr-2"
-    >
-      <ListboxButton className="flex w-full items-center overflow-clip text-nowrap rounded-md border border-gray-300 bg-white p-2 text-left text-sm data-[active]:border-blue-400 data-[focus]:border-blue-300 data-[hover]:border-blue-300">
-        <p className="max-w-4/5 truncate">{selectedOption.text}</p>
-        <PiCaretDownBold className="font-semiboldbold ml-auto text-sm transition-all" />
-      </ListboxButton>
-      <ListboxOptions
-        anchor="bottom"
-        transition
-        className="w-[var(--button-width)] origin-top rounded-md bg-white transition duration-200 ease-out data-[closed]:scale-95 data-[enter]:border-blue-300 data-[closed]:opacity-0"
+    <Field className="w-full">
+      <Listbox
+        value={selectedOption}
+        onChange={handleSelect}
+        name={name}
+        defaultValue={selectedOption}
+        as="div"
+        className="relative mx-1 my-5 flex h-[38px] w-full pr-2"
       >
-        {allOptions.map((option) => (
-          <ListboxOption
-            key={option.id}
-            value={option}
-            className="border-l-4 border-transparent p-1 text-left data-[selected]:border-sky-400 data-[focus]:bg-sky-200"
-          >
-            <p>{option.text}</p>
-          </ListboxOption>
-        ))}
-      </ListboxOptions>
-    </Listbox>
+        {({ open }) => (
+          <>
+            <ListboxButton
+              id={name}
+              className={`flex w-full items-center overflow-clip text-nowrap rounded-md border border-gray-300 bg-white p-2 text-left text-sm hover:border-sky-300 ${
+                open ? "border-blue-300" : "border-gray-300"
+              }`}
+            >
+              <p className="max-w-4/5 truncate">{t(selectedOption.text)}</p>
+              <PiCaretDownBold className="font-semiboldbold ml-auto text-sm transition-all" />
+            </ListboxButton>
+            <ListboxOptions
+              anchor="bottom"
+              className="absolute z-10 w-[var(--button-width)] origin-top rounded-md bg-white transition duration-200 ease-out data-[closed]:scale-95 data-[enter]:border-blue-300 data-[closed]:opacity-0"
+            >
+              {allOptions.map((option) => (
+                <ListboxOption
+                  key={option.id}
+                  value={option}
+                  className="border-l-4 border-transparent p-1 text-left data-[selected]:border-sky-400 data-[focus]:bg-sky-200"
+                >
+                  <p>{t(option.text)}</p>
+                </ListboxOption>
+              ))}
+            </ListboxOptions>
+          </>
+        )}
+      </Listbox>
+    </Field>
   );
 }
