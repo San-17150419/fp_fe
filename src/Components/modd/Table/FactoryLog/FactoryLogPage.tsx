64407f08a8f1a5b1baa3122c84d724aa2d7490d6
ZENG-JING-YUAN <steven.tseng@heavypower.com.tw>
2024-07-11 17:06:49 +0800

    feat: Add translation support and improve layout

--- src/Components/modd/Table/FactoryLog/FactoryLogPage.tsx
+++ src/Components/modd/Table/FactoryLog/FactoryLogPage.tsx
@@ -3,14 +3,19 @@ import FactoryLogPreFilter from "./FactoryLogPreFilter";
 import FactoryLogPostFilter from "./FactoryLogPostFilter";
 import FactoryLogTable from "./FactoryLogTable";
 import TableContainer from "../TableContainer";
+import { useTranslation } from "react-i18next";
 
 export default function FactoryLog() {
+  const { t } = useTranslation();
   return (
     <FactoryLogContextProvider>
-      <div className="flex h-full grow flex-col flex-wrap gap-4 p-2 outline">
+      <div className="flex h-full grow flex-col flex-wrap gap-4 p-2">
         <FactoryLogPreFilter />
-        <FactoryLogPostFilter />
-        <TableContainer>
+        <div className="border-b-2 border-gray-500 p-2 text-center text-xl font-extrabold text-gray-700">
+          {t("生產部門")} {t("達成率")}
+        </div>
+        <TableContainer className="text-gray-700 outline">
+          <FactoryLogPostFilter />
           <FactoryLogTable />
         </TableContainer>
       </div>
