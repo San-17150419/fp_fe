b8c373078ce13cc80f8f5af24ff4a7d96b40904a
ZENG-JING-YUAN <steven.tseng@heavypower.com.tw>
2024-07-11 16:59:23 +0800

    refactor: improve FactoryLog filters - Added translation support - Improved general layouts

--- src/Components/modd/Table/FactoryLog/FactoryLogPostFilter.tsx
+++ src/Components/modd/Table/FactoryLog/FactoryLogPostFilter.tsx
@@ -1,10 +1,8 @@
-import React from "react";
 import { useFactoryLogContext } from "./FactoryLogContext";
 import Select from "../../Select/Select";
 import { useTranslation } from "react-i18next";
 export default function FactoryLogPostFilter() {
   const {
-    rawData,
     isPostDataReady,
     departmentList,
     isRequestMade,
@@ -14,17 +12,15 @@ export default function FactoryLogPostFilter() {
   if (!isRequestMade) return null;
   if (!isPostDataReady) return <div>Loading...</div>;
   return (
-    <section className="min-h-[100px] outline">
-      <div className="w-1/4 pl-2 pr-3">
-        <Select
-          onSelect={(value) => setCurrentDepartment(value as string)}
-          options={departmentList.map((department) => ({
-            value: department,
-            text: t(department),
-          }))}
-          name="department"
-        />
-      </div>
-    </section>
+    <div className="w-1/4 pl-2 pr-3">
+      <Select
+        onSelect={(value) => setCurrentDepartment(value as string)}
+        options={departmentList.map((department) => ({
+          value: department,
+          text: t(department),
+        }))}
+        name="department"
+      />
+    </div>
   );
 }
--- src/Components/modd/Table/FactoryLog/FactoryLogPreFilter.tsx
+++ src/Components/modd/Table/FactoryLog/FactoryLogPreFilter.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { useFactoryLogContext } from "./FactoryLogContext";
+import { useTranslation } from "react-i18next";
 import Select from "../../Select/Select";
 import { PostDataParams } from "./FactoryLogDataType";
 
@@ -12,12 +13,10 @@ export default function FactoryLogPreFilter() {
     point?: "ar" | "pamt_p";
     [key: string]: string | undefined;
   }>({});
-
+  const { t } = useTranslation();
   const [dateStart, setDateStart] = useState<string>(
     new Date().toISOString().slice(0, 10),
   );
-
-  if (!isPreDataReady) return <div>Loading...</div>;
 
   function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
@@ -50,38 +49,53 @@ export default function FactoryLogPreFilter() {
       [key]: value,
     }));
   };
-
+  if (!isPreDataReady) return <div>{t("Loading...")}</div>;
   return (
-    <section className="min-h-[100px] outline">
+    <section className="min-h-[100px] pr-10 outline">
       <form
         id="form"
         onSubmit={handleSubmit}
-        className="grid grid-cols-4 place-items-center gap-4 p-3"
+        className="grid grid-cols-2 place-items-center gap-2 desktop:grid-cols-3"
       >
         {Object.entries(preData).map(([key, value]) => {
           if (key === "dep") {
             return null;
           }
           return (
-            <Select
+            <div
               key={key}
-              name={key}
-              onSelect={(value) => handleSelectChange(key, value)}
-              options={Object.entries(value).map(([key, value]) => ({
-                value: key,
-                text: value,
-              }))}
-            />
+              className="flex w-full items-center justify-end gap-4"
+            >
+              <label className="text-sm" htmlFor={key}>
+                {t(key)}
+              </label>
+              <div className="w-3/4">
+                <Select
+                  key={`key-${key}`}
+                  name={key}
+                  onSelect={(value) => handleSelectChange(key, value)}
+                  options={Object.entries(value).map(([key, value]) => ({
+                    value: key,
+                    text: value,
+                  }))}
+                />
+              </div>
+            </div>
           );
         })}
-
-        <input
-          type="date"
-          defaultValue={dateStart}
-          className="h-[38px] w-full rounded border px-4 hover:border-blue-400"
-          name="date"
-          onChange={(e) => setDateStart(e.target.value)}
-        />
+        <div className="flex w-full items-center justify-end gap-4 text-nowrap">
+          <label htmlFor="date">{t("當期開始")}</label>
+          <div className="w-3/4 px-1">
+            <input
+              type="date"
+              defaultValue={dateStart}
+              className="h-[38px] w-full rounded border px-4 hover:border-blue-400"
+              name="date"
+              id="date"
+              onChange={(e) => setDateStart(e.target.value)}
+            />
+          </div>
+        </div>
       </form>
       <div className="m-2 block">
         <button
@@ -89,7 +103,7 @@ export default function FactoryLogPreFilter() {
           type="submit"
           form="form"
         >
-          Search
+          {t("Search")}
         </button>
       </div>
     </section>
