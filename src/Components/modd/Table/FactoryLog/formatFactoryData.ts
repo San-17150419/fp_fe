64774854389f46b911cc259be2e39aa39e715f7b
ZENG-JING-YUAN <steven.tseng@heavypower.com.tw>
2024-07-11 17:04:19 +0800

    feat: add utility functions to format factory log
    - Added formatFactoryLogData and transformData utility functions in
    the new file formatFactoryData.ts.
    
    - These functions process raw factory log data into structured formats
    suitable for table rendering.

---
+++ src/Components/modd/Table/FactoryLog/formatFactoryData.ts
@@ -0,0 +1,95 @@
+import { FactoryLogRawData, LogData } from "./FactoryLogDataType";
+
+type FormattedData = {
+  [key: string]: any;
+};
+
+export const formatFactoryLogData = (
+  rawData: FactoryLogRawData,
+): FormattedData[] => {
+  const tableData: FormattedData[] = [];
+
+  rawData.data.forEach((item) => {
+    const { dep, data } = item;
+    const departmentData: FormattedData[] = [];
+    // sort date ranges by date_start in ascending order
+    const sortedData = data.sort((a, b) => {
+      return (
+        new Date(a.date_start).getTime() - new Date(b.date_start).getTime()
+      );
+    });
+
+    sortedData.forEach((dateRange) => {
+      const { date_start, raw } = dateRange;
+      raw.forEach((entry) => {
+        const { sys, ar } = entry;
+        let row = departmentData.find((r) => r.sys === sys);
+        if (!row) {
+          row = { sys };
+          departmentData.push(row);
+        }
+
+        row[date_start] = ar;
+      });
+    });
+    tableData.push({ [dep]: departmentData });
+  });
+  return tableData;
+};
+
+// function formatDateToQuarter(date: Date): string {
+//       const year = date.getFullYear();
+//       const month = date.getMonth(); // getMonth() returns 0-11
+//       const quarter = Math.floor(month / 3) + 1; // Calculate the quarter
+//       return `${year}-Q${quarter}`;
+//     }
+
+//     function getHeaders() {
+//       const header = ["系統"];
+//       if (rawData) {
+//         console.dir(rawData);
+//         rawData.duration.map(({ date_start }) => {
+//           header.push(formatDateToQuarter(new Date(date_start)));
+//         });
+//         header.push("比較其他季度");
+//       }
+
+//       return header;
+//     }
+export const transformData = (data: LogData[]) => {
+  const finalDataFormat: { [key: string]: any } = {};
+
+  data.forEach((item) => {
+    const { dep, data: dateRanges } = item;
+
+    if (!finalDataFormat[dep]) {
+      finalDataFormat[dep] = {};
+    }
+
+    const sortedData = dateRanges.sort((a, b) => {
+      return (
+        new Date(a.date_start).getTime() - new Date(b.date_start).getTime()
+      );
+    });
+
+    sortedData.forEach((dateRange) => {
+      const { raw } = dateRange;
+
+      raw.forEach((entry) => {
+        const { sys, pl, ...points } = entry;
+
+        Object.keys(points).forEach((point) => {
+          if (!finalDataFormat[dep][point]) {
+            finalDataFormat[dep][point] = {};
+          }
+          if (!finalDataFormat[dep][point][sys]) {
+            finalDataFormat[dep][point][sys] = [];
+          }
+
+          finalDataFormat[dep][point][sys].push(points[point]);
+        });
+      });
+    });
+  });
+  return finalDataFormat;
+};
