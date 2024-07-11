08c04013adfc6f73bffcd964e4a82992d09f4bea
ZENG-JING-YUAN <steven.tseng@heavypower.com.tw>
2024-07-11 16:28:30 +0800

    feat:update i18n configuration
    Updated the i18n configuration in i18n.js to include the new language
    resource and set it as the default language.

--- src/i18n.js
+++ src/i18n.js
@@ -2,26 +2,31 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import translationZhTw from "./assets/locales/zh-tw/translation.json";
 import translationEn from "./assets/locales/en/translation.json";
+import translationZhCn from "./assets/locales/zh-cn/translation.json";
 
 const resources = {
   en: {
     translation: translationEn,
   },
-  zh: {
+  "zh-TW": {
     translation: translationZhTw,
+  },
+  "zh-CN": {
+    translation: translationZhCn,
   },
 };
 
 i18n
-  .use(initReactI18next) // passes i18n down to react-i18next
+  .use(initReactI18next)
   .init({
     resources,
-    lng: "zh",
-    fallbackLng: "en",
+    lng: "zh-CN",
+    fallbackLng: "zh-TW",
     keySeparator: false,
     interpolation: {
       escapeValue: false,
     },
+    // debug: true, // Enable debug mode to see logs
   });
 
 export default i18n;
