3285fdd5c550631b01a974ca96774f47b6d8a78e
ZENG-JING-YUAN <steven.tseng@heavypower.com.tw>
2024-07-12 16:29:23 +0800

    feat: Refine type definition to improve type safety
    Add more detail type definition to increase type safety

--- src/Components/modd/Table/FactoryLog/FactoryLogDataType.ts
+++ src/Components/modd/Table/FactoryLog/FactoryLogDataType.ts
@@ -1,13 +1,16 @@
 type GenericObject = Record<string, any>;
 
 export type FactoryLogPreData = {
-  preData: {
-    factory: GenericObject;
-    point: GenericObject;
-    date_type: GenericObject;
-    dep: { [key: string]: GenericObject };
-  };
+  preData: PreData;
 };
+// export type FactoryLogPreData = {
+//   preData: {
+//     factory: GenericObject;
+//     point: GenericObject;
+//     date_type: GenericObject;
+//     dep: { [key: string]: GenericObject };
+//   };
+// };
 
 export type PostDataParams = {
   factory: "GD" | "HP" | "DL";
@@ -18,46 +21,109 @@ export type PostDataParams = {
 };
 
 export type LogData = {
-  dep: string;
+  dep: Factory;
   data: Array<{
     date_start: string;
     date_end: string;
-    raw: Array<{ [key: string]: number }>;
+    raw: RawData[];
   }>;
 };
 
 export type FactoryLogRawData = {
-  dep: string[];
-  duration: { date_start: string; date_end: string }[];
-  data: LogData[];
+  data: Temp[];
+  duration: Array<{ date_start: string; date_end: string }>;
   post: {
+    factory: Factory;
+    date_type: "half-year" | "quarter";
     date_start: string;
-    date_type: string;
-    factory: string;
-    // Right now, the return value of point is null, regardless of wether it was specified or not
-    point: string | null;
+    point: "ar" | "pamt_p" | null;
   };
 };
 
+type Temp = {
+  dep: Factory;
+  data: Array<{
+    date_start: string;
+    date_end: string;
+    raw: RawData[];
+  }>;
+};
+
 export type FactoryLogContextType = {
-  preData: FactoryLogPreData["preData"];
+  preData: FactoryLogPreData["preData"] | null;
   fetchRawData: (
     params: PostDataParams,
   ) => Promise<FactoryLogRawData | undefined>;
   isPreDataReady: boolean;
-  isRawDataReady: boolean;
-  rawData: FactoryLogRawData | null;
-  isPostDataReady: boolean;
-  departmentList: string[];
+  // isRawDataReady: boolean;
+  // rawData: FactoryLogRawData | null;
+  // isPostDataReady: boolean;
+  // departmentList: string[];
   isRequestMade: boolean;
   setIsRequestMade: React.Dispatch<React.SetStateAction<boolean>>;
-  currentDepartment: string;
-  setCurrentDepartment: React.Dispatch<React.SetStateAction<string>>;
-  processedData: FormattedData | null;
-  isProcessedDataReady: boolean;
+  // currentDepartment: string;
+  // setCurrentDepartment: React.Dispatch<React.SetStateAction<string>>;
+  // processedData: FormattedData | null;
+  // isProcessedDataReady: boolean;
   postData: Record<string, any>;
+  tableData: FormattedData | null;
+  isTableDataReady: boolean;
+  duration: Array<{ date_start: string; date_end: string }>;
 };
 
 export type FormattedData = {
   [key: string]: any;
 };
+
+export type Factory = "GD" | "HP" | "DL";
+
+export type Point =
+  | "ar"
+  | "pamt_p"
+  | "pamt_h"
+  | "wt"
+  | "vt"
+  | "sd"
+  | "pamt"
+  | "cpamt";
+
+export type DateType = "half-year" | "quarter";
+
+export type GD_Departments =
+  | "INJ"
+  | "CE"
+  | "MECH"
+  | "T1"
+  | "T2"
+  | "A89"
+  | "F99";
+export type HP_Departments =
+  | "INJ"
+  | "D09"
+  | "OEM"
+  | "CE"
+  | "MECH"
+  | "T1"
+  | "T2"
+  | "T3";
+export type DL_Departments = "INJ" | "MECH" | "ASM";
+export type DepartmentMap = {
+  GD: GD_Departments;
+  HP: HP_Departments;
+  DL: DL_Departments;
+};
+
+export type Department<F extends Factory> = DepartmentMap[F];
+
+export type PreData = {
+  factory: { [key in Factory]: string };
+  point: Partial<{ [key in Point]: string }>;
+  date_type: { [key in DateType]: string };
+  dep: {
+    [F in Factory]: { [D in Department<F>]: string };
+  };
+};
+
+export type RawData = Partial<{ [key in Point]: number }> & { ar: number } & {
+  pamt_p: number;
+} & { sys: string } & { pl: string };
