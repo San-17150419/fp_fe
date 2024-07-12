9833420fe213091c48af2fde3ebe11e6a4bcf3b0
ZENG-JING-YUAN <steven.tseng@heavypower.com.tw>
2024-07-12 16:37:57 +0800

    feat: Increase style flexibility

--- src/Components/modd/Select/Select.tsx
+++ src/Components/modd/Select/Select.tsx
@@ -10,17 +10,18 @@ import { useSelect } from "./useSelect";
 import { SelectOption, DetailOption } from "./selectType";
 import { useEffect, useCallback } from "react";
 import { useTranslation } from "react-i18next";
+import cn from "../../../util/cn";
 
 type SelectProps = {
   options: SelectOption[];
   name: string;
-  width?: string;
+  className?: string;
   onSelect?: (option: string) => void;
 };
 
 export default function Select({
   options,
-  width,
+  className,
   onSelect,
   name,
 }: SelectProps) {
@@ -40,43 +41,44 @@ export default function Select({
   };
 
   return (
-    <Field className="w-full">
-      <Listbox
-        value={selectedOption}
-        onChange={handleSelect}
-        name={name}
-        defaultValue={selectedOption}
-        as="div"
-        className="relative mx-1 my-5 flex h-[38px] w-full pr-2"
-      >
-        {({ open }) => (
-          <>
-            <ListboxButton
-              id={name}
-              className={`flex w-full items-center overflow-clip text-nowrap rounded-md border border-gray-300 bg-white p-2 text-left text-sm hover:border-sky-300 ${
-                open ? "border-blue-300" : "border-gray-300"
-              }`}
-            >
-              <p className="max-w-4/5 truncate">{t(selectedOption.text)}</p>
-              <PiCaretDownBold className="font-semiboldbold ml-auto text-sm transition-all" />
-            </ListboxButton>
-            <ListboxOptions
-              anchor="bottom"
-              className="absolute z-10 w-[var(--button-width)] origin-top rounded-md bg-white transition duration-200 ease-out data-[closed]:scale-95 data-[enter]:border-blue-300 data-[closed]:opacity-0"
-            >
-              {allOptions.map((option) => (
-                <ListboxOption
-                  key={option.id}
-                  value={option}
-                  className="border-l-4 border-transparent p-1 text-left data-[selected]:border-sky-400 data-[focus]:bg-sky-200"
-                >
-                  <p>{t(option.text)}</p>
-                </ListboxOption>
-              ))}
-            </ListboxOptions>
-          </>
-        )}
-      </Listbox>
-    </Field>
+    <Listbox
+      value={selectedOption}
+      onChange={handleSelect}
+      name={name}
+      defaultValue={selectedOption}
+      as="div"
+      className={cn(
+        "relative mx-1 my-2 flex h-7 w-full pr-2 text-xs desktop:text-base",
+        className,
+      )}
+    >
+      {({ open }) => (
+        <>
+          <ListboxButton
+            id={name}
+            className={`flex w-full items-center overflow-clip text-nowrap rounded-md border border-gray-300 bg-white px-2 py-0 text-left text-xs hover:border-sky-300 desktop:px-3 desktop:py-4 desktop:text-base ${
+              open ? "border-blue-300" : "border-gray-300"
+            }`}
+          >
+            <p className="max-w-4/5 truncate">{t(selectedOption.text)}</p>
+            <PiCaretDownBold className="font-semiboldbold ml-auto text-sm transition-all" />
+          </ListboxButton>
+          <ListboxOptions
+            anchor="bottom"
+            className="absolute z-10 w-[var(--button-width)] origin-top rounded-md bg-white text-xs transition duration-200 ease-out data-[closed]:scale-95 data-[enter]:border-blue-300 data-[closed]:opacity-0 desktop:text-base"
+          >
+            {allOptions.map((option) => (
+              <ListboxOption
+                key={option.id}
+                value={option}
+                className="border-l-4 border-transparent p-2 text-left text-xs data-[selected]:border-sky-400 data-[focus]:bg-sky-200 desktop:text-base"
+              >
+                <p>{t(option.text)}</p>
+              </ListboxOption>
+            ))}
+          </ListboxOptions>
+        </>
+      )}
+    </Listbox>
   );
 }
