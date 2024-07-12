7baffa31a4cc4333a69d1ee6e76b9b1a451070e3
ZENG-JING-YUAN <steven.tseng@heavypower.com.tw>
2024-07-12 16:33:25 +0800

    Refactor: Remove unused code and import

--- src/Components/modd/Table/FactoryLog/FactoryLogPage.tsx
+++ src/Components/modd/Table/FactoryLog/FactoryLogPage.tsx
@@ -1,23 +1,13 @@
 import { FactoryLogContextProvider } from "./FactoryLogContext";
 import FactoryLogPreFilter from "./FactoryLogPreFilter";
-import FactoryLogPostFilter from "./FactoryLogPostFilter";
 import FactoryLogTable from "./FactoryLogTable";
-import TableContainer from "../TableContainer";
-import { useTranslation } from "react-i18next";
 
 export default function FactoryLog() {
-  const { t } = useTranslation();
   return (
     <FactoryLogContextProvider>
-      <div className="flex h-full grow flex-col flex-wrap gap-4 p-2">
+      <div className="mx-8 my-4 flex h-full grow flex-col flex-wrap gap-4 p-2 desktop:mx-20">
         <FactoryLogPreFilter />
-        <div className="border-b-2 border-gray-500 p-2 text-center text-xl font-extrabold text-gray-700">
-          {t("生產部門")} {t("達成率")}
-        </div>
-        <TableContainer className="text-gray-700 outline">
-          <FactoryLogPostFilter />
-          <FactoryLogTable />
-        </TableContainer>
+        <FactoryLogTable />
       </div>
     </FactoryLogContextProvider>
   );
