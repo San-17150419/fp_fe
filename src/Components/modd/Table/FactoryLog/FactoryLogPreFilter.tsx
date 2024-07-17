6d371cfb297ae13582cec666bc82e87c9e7697bb
ZENG-JING-YUAN <steven.tseng@heavypower.com.tw>
2024-07-17 17:09:22 +0800

    feat: Improve UI of FactoryLogPreFilter
    - Added the GrSearch icon to the submit button to make the user
    interaction more intuitive
    
    - Adjusted the layout to make it more compact.

--- src/Components/modd/Table/FactoryLog/FactoryLogPreFilter.tsx
+++ src/Components/modd/Table/FactoryLog/FactoryLogPreFilter.tsx
@@ -4,8 +4,10 @@ import { useTranslation } from "react-i18next";
 import Select from "../../Select/Select";
 import { PostDataParams } from "./FactoryLogDataType";
 import InputBase from "../../Input/InputBase";
-
+import { GrSearch } from "react-icons/gr";
 export default function FactoryLogPreFilter() {
+  // TODO: Maybe I need to change the default value of selectedDate to "yesterday". When the dafault value is today, the data in fourth interval is always missing. It could be a mistake from backend. Possible solution: 1. Set the default value to yesterday. 2. Change backend data or api. 3. Maybe fetch 5 intervals? Or some other way to increase or even make sure the data is not missing.
+
   const { preData, isPreDataReady, fetchRawData, setIsRequestMade } =
     useFactoryLogContext();
   const [selectedValues, setSelectedValues] = useState<{
@@ -52,11 +54,11 @@ export default function FactoryLogPreFilter() {
   if (!isPreDataReady) return <div>{t("Loading...")}</div>;
   return (
     <>
-      <section className="min-h-[100px] p-2">
+      <section className="flex min-h-[100px] p-2">
         <form
           id="form"
           onSubmit={handleSubmit}
-          className="grid grid-cols-4 place-items-center gap-2"
+          className="grid w-full grid-cols-9 place-items-center gap-1"
         >
           {preData &&
             Object.entries(preData).map(([key, value]) => {
@@ -64,14 +66,17 @@ export default function FactoryLogPreFilter() {
                 return null;
               }
               return (
-                <div key={key} className="flex w-full flex-col">
-                  <label className="ml-3 mt-2 text-xs" htmlFor={key}>
+                <div key={key} className="col-span-2 flex w-full flex-col">
+                  <label
+                    className="ml-3 mt-2 text-xs desktop:text-sm"
+                    htmlFor={key}
+                  >
                     {t(key)}
                   </label>
                   <Select
                     key={`key-${key}`}
                     name={key}
-                    className="my-1"
+                    className="my-1 font-semibold"
                     onSelect={(value) => handleSelectChange(key, value)}
                     options={Object.entries(value).map(([key, value]) => ({
                       value: key,
@@ -81,8 +86,8 @@ export default function FactoryLogPreFilter() {
                 </div>
               );
             })}
-          <div className="flex w-full flex-col">
-            <label className="ml-2 mt-2 text-xs" htmlFor="date">
+          <div className="col-span-2 flex w-full flex-col">
+            <label className="ml-2 mt-2 text-xs desktop:text-sm" htmlFor="date">
               {t("當期開始")}
             </label>
             <InputBase
@@ -90,19 +95,31 @@ export default function FactoryLogPreFilter() {
               defaultValue={dateStart}
               name="date"
               id="date"
+              className="font-semibold"
               onChange={(e) => setDateStart(e.target.value)}
             />
           </div>
+          <div className="col-span-1 flex w-full flex-col">
+            <button
+              title="Search"
+              className="ml-auto mt-6 flex h-[1.875rem] w-[1.875rem] items-center justify-center rounded border bg-gray-400 text-xs hover:bg-gray-500 hover:text-white desktop:mt-8 desktop:h-9 desktop:w-9 desktop:text-sm"
+              type="submit"
+              form="form"
+            >
+              <GrSearch />
+            </button>
+          </div>
         </form>
-        <div className="m-2 block">
+        {/* <div className="flex flex-grow pt-9">
           <button
-            className="ml-auto block rounded border border-gray-700 bg-gray-400 p-2 text-xs hover:bg-gray-500 hover:text-white"
+            title="Search"
+            className="ml-2 flex h-8 w-8 items-center justify-center rounded border text-xs hover:bg-gray-500 hover:text-white desktop:text-sm"
             type="submit"
             form="form"
           >
-            {t("Search")}
+            <GrSearch />
           </button>
-        </div>
+        </div> */}
       </section>
     </>
   );
