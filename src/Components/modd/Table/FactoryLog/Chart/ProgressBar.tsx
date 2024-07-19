22682698cc754a476df312d4ce9797a2178cc67c
ZENG-JING-YUAN <steven.tseng@heavypower.com.tw>
2024-07-19 14:33:39 +0800

    feat: Enhance ProgressBar component
    Added useRef and useEffect to set ARIA attributes on the ProgressBar
    component, improving accessibility. Calculated progress percentage
    separately for cleaner code.

--- src/Components/modd/Table/FactoryLog/Chart/ProgressBar.tsx
+++ src/Components/modd/Table/FactoryLog/Chart/ProgressBar.tsx
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 
 type ProgressBarProps = {
   target: number;
   achieved: number;
 };
 export default function ProgressBar({ target, achieved }: ProgressBarProps) {
+  const progressBarRef = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    if (progressBarRef.current) {
+      progressBarRef.current.setAttribute("aria-valuenow", String(achieved));
+      progressBarRef.current.setAttribute("aria-valuemax", String(target));
+      progressBarRef.current.setAttribute("aria-valuemin", "0");
+    }
+  }, [achieved, target]);
+
+  const progressPercentage = (achieved / target) * 100;
   return (
     <div className="flex flex-col gap-1 pr-8">
       <div className="flex justify-between">
@@ -13,11 +23,13 @@ export default function ProgressBar({ target, achieved }: ProgressBarProps) {
       </div>
       <div className="flex gap-2 rounded bg-gray-300">
         <div
+          aria-label={`Progress: ${progressPercentage}%`}
+          role="progressbar"
           className="relative h-6 rounded bg-gray-600"
-          style={{ width: `${(achieved / target) * 100}%` }}
+          style={{ width: `${progressPercentage}%` }}
         >
           <span className="absolute right-1.5 top-1/2 -translate-y-1/2 text-xs text-gray-200">
-            {((achieved / target) * 100).toFixed(1)}%
+            {progressPercentage.toFixed(1)}%
           </span>
         </div>
       </div>
