483414774fa4b5d87bff8a5e19da84d6e300bd22
ZENG-JING-YUAN <steven.tseng@heavypower.com.tw>
2024-07-19 14:25:58 +0800

    feat: Enhance useCloseAndEscape hook
    Added focus management to the useCloseAndEscape hook to ensure the
    trigger element is focused when closing. This enhancement prevents the
    focus from being lost when clicking outside of the referenced element.

--- src/hooks/useCloseAndEscape.ts
+++ src/hooks/useCloseAndEscape.ts
@@ -1,12 +1,29 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 
+/**
+ * A custom hook that adds event listeners to the window for both click and escape key presses.
+ * If the click event occurs outside of the element referenced by `ref`, or if the escape key is pressed,
+ * the `onClose` function is called. Focus is also managed to ensure the trigger element is focused when closing.
+ *
+ * @param {React.RefObject<HTMLElement>} ref - A reference to the element that should be considered the boundary for the click event.
+ * @param {() => void} onClose - A function that should be called when a click event occurs outside of the element referenced by `ref`, or when the escape key is pressed.
+ * @param {boolean} isOpen - A boolean that indicates if the element is open and should be focusable.
+ * @return {void} This function does not return anything.
+ */
 export default function useCloseAndEscape(
   ref: React.RefObject<HTMLElement>,
   onClose: () => void,
+  isOpen: boolean,
 ) {
+  const triggerRef = useRef<HTMLElement | null>(null);
+
   useEffect(() => {
     const handleClickOutside = (event: MouseEvent) => {
       if (ref.current && !ref.current.contains(event.target as Node)) {
+        // This is for preventing element other than the trigger element from being focused when closing
+        // Without this, the focus would be lost when clicking outside of the element
+        event.preventDefault();
+        event.stopPropagation();
         onClose();
       }
     };
@@ -17,12 +34,26 @@ export default function useCloseAndEscape(
       }
     };
 
-    window.addEventListener("mousedown", handleClickOutside);
-    window.addEventListener("keydown", handleEscape);
+    if (isOpen) {
+      // Save the currently focused element
+      triggerRef.current = document.activeElement as HTMLElement;
+
+      window.addEventListener("mousedown", handleClickOutside);
+      window.addEventListener("keydown", handleEscape);
+
+      if (ref.current && typeof ref.current.focus === "function") {
+        ref.current.focus();
+      }
+    }
 
     return () => {
       window.removeEventListener("mousedown", handleClickOutside);
       window.removeEventListener("keydown", handleEscape);
+
+      // Restore focus to the trigger element
+      if (triggerRef.current) {
+        triggerRef.current.focus();
+      }
     };
-  }, [ref, onClose]);
+  }, [ref, onClose, isOpen]);
 }
