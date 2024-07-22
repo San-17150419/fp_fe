a4c7185bc92a2d1bbae860f10b19e9667065a502
ZENG-JING-YUAN <steven.tseng@heavypower.com.tw>
2024-07-22 17:00:56 +0800

    feat: Add new ProgressChart component
    A new component for rendering a collection of progress bar.
    - Generates progress chart data from `FactoryEventReponse`.
    - Renders progress bars with appropriate titles, targets, and
    achievements.

---
+++ src/Components/modd/Table/FactoryLog/Chart/ProgressChart.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import ProgressBar from "./ProgressBar";
+import { type FactoryEventReponse } from "../FactoryLogDataType";
+type ProgressChartProps = {
+  eventData: FactoryEventReponse;
+  children?: React.ReactNode;
+};
+export default function ProgressChart({
+  eventData,
+  children,
+}: ProgressChartProps) {
+  function generateProgressChartData() {
+    const data: { title: string; target: number; achieved: number }[] = [];
+    eventData.data.forEach((item) => {
+      data.push({
+        title: item.prod_name,
+        target: item.cpamt,
+        achieved: item.pamt,
+      });
+    });
+
+    return data;
+  }
+
+  return (
+    <div >
+      {children}
+      <div className="w-[90% mx-2 my-8 grid grid-cols-2 px-2 py-4">
+        {generateProgressChartData().map((item) => (
+          <ProgressBar
+            key={item.title}
+            title={item.title}
+            target={item.target}
+            achieved={item.achieved}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
