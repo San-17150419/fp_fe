f43493759b16efb1920e969cdd1c74e6ccad43d2
ZENG-JING-YUAN <steven.tseng@heavypower.com.tw>
2024-07-22 16:47:55 +0800

    fix:Optimize state management and event handling
    Fixed an issue where FactoryLogPreFilter rerenders exceexcessively. This
    is caused by `handleSelectChange` function. Somehow, memoizing it using
    useCallback does not solve the issue. A total rework is done.
    
    - Replaced the single `selectedValues` state object with individual
    states for `selectedFactory`, `selectedDateType`, and `selectedPoint`.
    - Added `useMemo` for memoizing preData options to avoid
    unnecessary re-renders.
    - Utilized `useCallback` for handling form submission and select
    changes to optimize performance.
    - Enhanced button and input styles for better user experience.

--- src/Components/modd/Table/FactoryLog/FactoryLogPreFilter.tsx
+++ src/Components/modd/Table/FactoryLog/FactoryLogPreFilter.tsx
@@ -1,126 +1,142 @@
-import React, { useState } from "react";
+import React, { useState, useMemo, useCallback } from "react";
 import { useFactoryLogContext } from "./FactoryLogContext";
 import { useTranslation } from "react-i18next";
 import Select from "../../Select/Select";
 import { PostDataParams } from "./FactoryLogDataType";
 import InputBase from "../../Input/InputBase";
 import { GrSearch } from "react-icons/gr";
-export default function FactoryLogPreFilter() {
-  // TODO: Maybe I need to change the default value of selectedDate to "yesterday". When the dafault value is today, the data in fourth interval is always missing. It could be a mistake from backend. Possible solution: 1. Set the default value to yesterday. 2. Change backend data or api. 3. Maybe fetch 5 intervals? Or some other way to increase or even make sure the data is not missing.
 
+export default function FactoryLogPreFilter() {
   const { preData, isPreDataReady, fetchRawData, setIsRequestMade } =
     useFactoryLogContext();
-  const [selectedValues, setSelectedValues] = useState<{
-    factory?: "GD" | "HP" | "DL";
-    date_type?: "half-year" | "quarter";
-    point?: "ar" | "pamt_p";
-    [key: string]: string | undefined;
-  }>({});
+  const [selectedFactory, setSelectedFactory] = useState<"GD" | "HP" | "DL">(
+    "GD",
+  );
+  const [selectedDateType, setSelectedDateType] = useState<
+    "half-year" | "quarter"
+  >("half-year");
+  const [selectedPoint, setSelectedPoint] = useState<"ar" | "pamt_p">("ar");
   const { t } = useTranslation();
   const [dateStart, setDateStart] = useState<string>(
     new Date().toISOString().slice(0, 10),
   );
-
-  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
-    event.preventDefault();
-
-    if (
-      !selectedValues.factory ||
-      !selectedValues.date_type ||
-      !selectedValues.point
-    ) {
-      console.error("Missing required fields");
-      return;
-    }
-
-    const params: PostDataParams = {
-      date_start: dateStart,
-      factory: selectedValues.factory,
-      date_type: selectedValues.date_type,
-      point: selectedValues.point,
-      ...selectedValues,
-    };
-
-    fetchRawData(params);
-    setIsRequestMade(true);
-  }
-
-  const handleSelectChange = (key: string, value: string) => {
-    setSelectedValues((prevState) => ({
-      ...prevState,
-      [key]: value,
-    }));
+  const selectHandlers = {
+    factory: setSelectedFactory,
+    date_type: setSelectedDateType,
+    point: setSelectedPoint,
   };
-  if (!isPreDataReady) return <div>{t("Loading...")}</div>;
+
+  const handleSubmit = useCallback(
+    (event: React.FormEvent<HTMLFormElement>) => {
+      event.preventDefault();
+
+      if (!selectedFactory || !selectedDateType || !selectedPoint) {
+        console.error("Missing required fields");
+        return;
+      }
+
+      const params: PostDataParams = {
+        date_start: dateStart,
+        factory: selectedFactory,
+        date_type: selectedDateType,
+        point: selectedPoint,
+      };
+
+      fetchRawData(params);
+      setIsRequestMade(true);
+    },
+    [dateStart, fetchRawData, setIsRequestMade],
+  );
+
+  const memoizedOptions = useMemo(() => {
+    if (!preData) return {};
+    return Object.entries(preData).reduce(
+      (acc, [key, value]) => {
+        if (key === "dep") return acc;
+        acc[key] = Object.entries(value).map(([key, value]) => ({
+          value: key,
+          text: value,
+        }));
+        return acc;
+      },
+      {} as { [key: string]: { value: string; text: string }[] },
+    );
+  }, [preData]);
+  const handleFactoryChange = useCallback((value: "GD" | "HP" | "DL") => {
+    setSelectedFactory(value);
+  }, []);
+
+  const handleDateTypeChange = useCallback((value: "half-year" | "quarter") => {
+    setSelectedDateType(value);
+  }, []);
+
+  const handlePointChange = useCallback((value: "ar" | "pamt_p") => {
+    setSelectedPoint(value);
+  }, []);
   return (
-    <>
-      <section className="flex min-h-[100px] p-2">
-        <form
-          id="form"
-          onSubmit={handleSubmit}
-          className="grid w-full grid-cols-9 place-items-center gap-1"
-        >
-          {preData &&
-            Object.entries(preData).map(([key, value]) => {
-              if (key === "dep") {
-                return null;
-              }
-              return (
-                <div key={key} className="col-span-2 flex w-full flex-col">
-                  <label
-                    className="ml-3 mt-2 text-xs desktop:text-sm"
-                    htmlFor={key}
-                  >
-                    {t(key)}
-                  </label>
-                  <Select
-                    key={`key-${key}`}
-                    name={key}
-                    className="my-1 font-semibold"
-                    onSelect={(value) => handleSelectChange(key, value)}
-                    options={Object.entries(value).map(([key, value]) => ({
-                      value: key,
-                      text: value,
-                    }))}
-                  />
-                </div>
-              );
-            })}
-          <div className="col-span-2 flex w-full flex-col">
-            <label className="ml-2 mt-2 text-xs desktop:text-sm" htmlFor="date">
-              {t("當期開始")}
-            </label>
-            <InputBase
-              type="date"
-              defaultValue={dateStart}
-              name="date"
-              id="date"
-              className="font-semibold"
-              onChange={(e) => setDateStart(e.target.value)}
-            />
-          </div>
-          <div className="col-span-1 flex w-full flex-col">
-            <button
-              title="Search"
-              className="ml-auto mt-6 flex h-[1.875rem] w-[1.875rem] items-center justify-center rounded border bg-gray-400 text-xs hover:bg-gray-500 hover:text-white desktop:mt-8 desktop:h-9 desktop:w-9 desktop:text-sm"
-              type="submit"
-              form="form"
-            >
-              <GrSearch />
-            </button>
-          </div>
-        </form>
-        {/* <div className="flex flex-grow pt-9">
+    <section className="flex min-h-[100px] p-2">
+      <form
+        id="form"
+        onSubmit={handleSubmit}
+        className="grid w-full grid-cols-9 place-items-center gap-1"
+      >
+        {Object.entries(memoizedOptions).map(([key, options]) => {
+          let onSelectHandler: (value: any) => void;
+          switch (key) {
+            case "factory":
+              onSelectHandler = handleFactoryChange;
+              break;
+            case "date_type":
+              onSelectHandler = handleDateTypeChange;
+              break;
+            case "point":
+              onSelectHandler = handlePointChange;
+              break;
+            default:
+              onSelectHandler = () => {};
+          }
+          return (
+            <div key={key} className="col-span-2 flex w-full flex-col">
+              <label
+                className="ml-3 mt-2 text-xs desktop:text-sm"
+                htmlFor={key}
+              >
+                {t(key)}
+              </label>
+              <Select
+                key={`key-${key}`}
+                name={key}
+                className="my-1 font-semibold"
+                onSelect={onSelectHandler}
+                options={options}
+              />
+            </div>
+          );
+        })}
+        <div className="col-span-2 flex w-full flex-col">
+          <label className="ml-2 mt-2 text-xs desktop:text-sm" htmlFor="date">
+            {t("當期開始")}
+          </label>
+          <InputBase
+            type="date"
+            defaultValue={dateStart}
+            name="date"
+            id="date"
+            className="font-semibold shadow shadow-slate-300"
+            onChange={(e) => setDateStart(e.target.value)}
+          />
+        </div>
+        <div className="col-span-1 flex w-full flex-col">
           <button
             title="Search"
-            className="ml-2 flex h-8 w-8 items-center justify-center rounded border text-xs hover:bg-gray-500 hover:text-white desktop:text-sm"
+            className="ml-auto mt-6 flex h-[1.875rem] w-[1.875rem] items-center justify-center rounded bg-gray-400 text-xs shadow shadow-gray-500 hover:bg-gray-500 hover:text-white hover:shadow-gray-800 focus:shadow focus:shadow-gray-800 desktop:mt-8 desktop:h-9 desktop:w-9 desktop:text-sm"
             type="submit"
             form="form"
           >
             <GrSearch />
           </button>
-        </div> */}
-      </section>
-    </>
+        </div>
+      </form>
+    </section>
   );
 }
