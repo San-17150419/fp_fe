5485ee857101b9c2fdf56a8faf06d798d48b692f
ZENG-JING-YUAN <steven.tseng@heavypower.com.tw>
2024-07-23 16:06:17 +0800

    refactor: Remove unused import and code

--- src/Components/modd/Table/FactoryLog/FactoryLogTable.tsx
+++ src/Components/modd/Table/FactoryLog/FactoryLogTable.tsx
@@ -1,21 +1,11 @@
-import React, { useState } from "react";
 import { useFactoryLogContext } from "./FactoryLogContext";
-import { useTranslation } from "react-i18next";
 import Loading from "../../../Loading";
 import FactoryTableComponent from "./FactoryTableComponent";
 
 export default function FactoryLogTable() {
   const { postData, duration, isRequestMade, isTableDataReady } =
     useFactoryLogContext();
-  const { t } = useTranslation();
-  const [isOpen, setIsOpen] = useState(false);
-  const [currentDepartment, setCurrentDepartment] = useState("");
-  // If a row  has missing data, it will be hidden in the table by default
-  // User can
-  const [showHiddenColumns, setShowHiddenColumns] = useState(false);
   if (!postData) return null;
-  const dateRange = ["1", "2", "3", "4"];
-  // TODO: Add key to table. React is complaining. I need to check where did I forget to add key
   if (!isRequestMade) return null;
   if (!isTableDataReady) return <Loading />;
   return (
