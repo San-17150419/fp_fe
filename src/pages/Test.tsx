afb728404525129b9c96512c0886972b6d041cd9
ZENG-JING-YUAN <steven.tseng@heavypower.com.tw>
2024-07-31 17:15:19 +0800

    feat: add PDF generation feature to Test page
    - Imported necessary components from `@react-pdf/renderer` for creating
    PDF documents.
    - Imported `axios` and `file-saver` for data fetching and file saving.
    - Defined a new `downloadPDF` function to generate and download a PDF
    file.
    - Added a sample PDF document with custom styles and content.
    - Included new styles for PDF rendering.
    
    Testing if `@react-pdf/renderer` is suitable to our use case.

--- src/pages/Test.tsx
+++ src/pages/Test.tsx
@@ -1,13 +1,26 @@
 import { FactoryLogContextProvider } from "../Components/modd/Table/FactoryLog/FactoryLogContext";
 import Container from "../Components/modd/Table/FactoryLog/Chart/Container";
 import BubbleChart from "../Components/modd/Table/FactoryLog/Chart/BubbleChart";
-import { useState, useRef } from "react";
+import {
+  Document,
+  Page,
+  Text,
+  View,
+  StyleSheet,
+  PDFViewer,
+  pdf,
+} from "@react-pdf/renderer";
+import { useState, useRef, useEffect } from "react";
+import axios from "axios";
+import { saveAs } from "file-saver";
 export default function Chart() {
   const [file, setFile] = useState<FileList | null>(null);
   const [previewImgUrls, setPreviewImgUrls] = useState<string[]>([]);
   const ref = useRef<HTMLInputElement>(null);
   const imgRef = useRef<HTMLImageElement>(null);
   const herfRef = useRef<HTMLAnchorElement>(null);
+
+
 
   function createCSV() {
     const rows = [
@@ -31,7 +44,7 @@ export default function Chart() {
     const encodedUri = encodeURI(csvContent);
     const link = document.createElement("a");
     link.setAttribute("href", encodedUri);
-    link.setAttribute("download", "data.text");
+    link.setAttribute("download", "data.csv");
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
@@ -80,7 +93,20 @@ export default function Chart() {
       };
     });
   };
-
+  const styles = StyleSheet.create({
+    page: {
+      backgroundColor: "white",
+      color: "white",
+    },
+    section: {
+      margin: 10,
+      padding: 10,
+    },
+    viewer: {
+      width: window.innerWidth, //the pdf viewer will take up all of the width and height
+      height: window.innerHeight,
+    },
+  });
   function returnFileSize(number: number) {
     if (number < 1e3) {
       return `${number} bytes`;
@@ -91,6 +117,153 @@ export default function Chart() {
     }
   }
 
+  const downloadPDF = async () => {
+    const fileName = "test.pdf";
+    const blob = await pdf(
+      <Document>
+        {/*render a single page*/}
+        <Page size="A4" orientation="landscape" style={styles.page}>
+          <View
+            style={{
+              display: "flex",
+              borderTop: "1px solid black",
+              flexDirection: "row",
+              flexGrow: 1,
+              justifyContent: "space-between",
+              alignItems: "center",
+            }}
+          >
+            <Text
+              style={{
+                color: "red",
+                border: "1px solid black",
+                flexGrow: 1,
+                alignContent: "center",
+              }}
+            >
+              213
+            </Text>
+            <Text
+              style={{
+                color: "red",
+                border: "1px solid black",
+                flexGrow: 1,
+                alignContent: "center",
+                alignItems: "center",
+                justifyContent: "center",
+                textAlign: "center",
+              }}
+            >
+              213
+            </Text>
+            <Text
+              style={{
+                color: "red",
+                border: "1px solid black",
+                flexGrow: 1,
+                alignContent: "center",
+              }}
+            >
+              213
+            </Text>
+            <Text
+              style={{
+                color: "red",
+                border: "1px solid black",
+                flexGrow: 1,
+                alignContent: "center",
+              }}
+            >
+              213
+            </Text>
+            <Text
+              style={{
+                color: "red",
+                border: "1px solid black",
+                flexGrow: 1,
+                alignContent: "center",
+              }}
+            >
+              213
+            </Text>
+          </View>
+          <View style={styles.section}>
+            <Text>World</Text>
+          </View>
+        </Page>
+        <Page size="A4" orientation="landscape" style={styles.page}>
+          <View
+            style={{
+              display: "flex",
+              borderTop: "1px solid black",
+              flexDirection: "row",
+              flexGrow: 1,
+              justifyContent: "space-between",
+              alignItems: "center",
+            }}
+          >
+            <Text
+              style={{
+                color: "red",
+                border: "1px solid black",
+                flexGrow: 1,
+                alignContent: "center",
+              }}
+            >
+              213
+            </Text>
+            <Text
+              style={{
+                color: "red",
+                border: "1px solid black",
+                flexGrow: 1,
+                alignContent: "center",
+                alignItems: "center",
+                justifyContent: "center",
+                textAlign: "center",
+              }}
+            >
+              213
+            </Text>
+            <Text
+              style={{
+                color: "red",
+                border: "1px solid black",
+                flexGrow: 1,
+                alignContent: "center",
+              }}
+            >
+              213
+            </Text>
+            <Text
+              style={{
+                color: "red",
+                border: "1px solid black",
+                flexGrow: 1,
+                alignContent: "center",
+              }}
+            >
+              213
+            </Text>
+            <Text
+              style={{
+                color: "red",
+                border: "1px solid black",
+                flexGrow: 1,
+                alignContent: "center",
+              }}
+            >
+              213
+            </Text>
+          </View>
+          <View style={styles.section}>
+            <Text>World</Text>
+          </View>
+        </Page>
+      </Document>,
+    ).toBlob();
+    saveAs(blob, fileName);
+  };
   return (
     <>
       {/* <BubbleChart /> */}
@@ -116,13 +289,99 @@ export default function Chart() {
       <div>
         {previewImgUrls.map((url, index) => (
           <>
-            <img alt="" src={url} key={url} />
+            <img alt="" src={url} key={url.slice(0, 10) + index} />
 
-            <a href={url} download={file?.[index].name}>
+            {/* <a href={url} download={file?.[index].name}>
               {file?.[index].name}
-            </a>
+            </a> */}
           </>
         ))}
+      </div>
+      <button
+        type="button"
+        onClick={() => downloadCSV(createCSV())}
+        className="border border-black"
+      >
+        download
+      </button>
+      <PDFViewer
+        style={styles.viewer}
+        // showToolbar={false}
+      >
+        {/* Start of the document*/}
+        <Document>
+          {/*render a single page*/}
+          <Page size="A4" orientation="landscape" style={styles.page}>
+            <View
+              style={{
+                display: "flex",
+                borderTop: "1px solid black",
+                flexDirection: "row",
+                flexGrow: 1,
+                justifyContent: "space-between",
+                alignItems: "center",
+              }}
+            >
+              <Text
+                style={{
+                  color: "red",
+                  border: "1px solid black",
+                  flexGrow: 1,
+                  alignContent: "center",
+                }}
+              >
+                213
+              </Text>
+              <Text
+                style={{
+                  color: "red",
+                  border: "1px solid black",
+                  flexGrow: 1,
+                  alignContent: "center",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  textAlign: "center",
+                }}
+              >
+                213
+              </Text>
+              <Text
+                style={{
+                  color: "red",
+                  border: "1px solid black",
+                  flexGrow: 1,
+                  alignContent: "center",
+                }}
+              >
+                213
+              </Text>
+              <Text
+                style={{
+                  color: "red",
+                  border: "1px solid black",
+                  flexGrow: 1,
+                  alignContent: "center",
+                }}
+              >
+                213
+              </Text>
+              <Text
+                style={{
+                  color: "red",
+                  border: "1px solid black",
+                  flexGrow: 1,
+                  alignContent: "center",
+                }}
+              >
+                213
+              </Text>
+            </View>
+          </Page>
+        </Document>
+      </PDFViewer>
+      ,
+      <div className="outline">
+        <button onClick={downloadPDF}>download pdf</button>
       </div>
     </>
   );
