b426cd97e5de0417f3633ed0b47a1487e056834d
ZENG-JING-YUAN <steven.tseng@heavypower.com.tw>
2024-08-02 17:21:43 +0800

    refactor: Adjuste App component layout a
    - Cleaned up commented-out code .
    - Adjusted the positioning of the language change buttons

--- src/App.tsx
+++ src/App.tsx
@@ -10,65 +10,43 @@ function App() {
   const changeLanguage = (lng: string) => {
     i18n.changeLanguage(lng);
   };
-  // const ref = useRef<HTMLDivElement>(null);
-  // const [size, setSize] = useState({ width: 0, height: 0 });
-  // const updateSize = () => {
-  //   if (ref.current) {
-  //     setSize({
-  //       width: ref.current.offsetWidth,
-  //       height: ref.current.offsetHeight,
-  //     });
-  //   }
-  // };
-
-  // useEffect(() => {
-  //   updateSize();
-  //   window.addEventListener("resize", updateSize);
-  //   return () => {
-  //     window.removeEventListener("resize", updateSize);
-  //   };
-  // }, []);
-
   return (
-    <div
-      // ref={ref}
-      className="mx-auto flex flex-col max-2xl:h-full lg:w-full desktop:min-h-[768px] desktop:min-w-[1366px] xl:max-h-[1080px] xl:max-w-[1920px] 2xl:h-[1080px] max-lg:portrait:max-h-[1280px] max-lg:portrait:min-h-[1024px] max-lg:portrait:min-w-[768px] max-lg:portrait:max-w-[800px] max-lg:landscape:h-[768px] max-lg:landscape:w-[1024px]"
-    >
+    <div className="mx-auto flex flex-col max-2xl:h-full lg:w-full desktop:min-h-[768px] desktop:min-w-[1366px] xl:max-h-[1080px] xl:max-w-[1920px] 2xl:h-[1080px] max-lg:portrait:max-h-[1280px] max-lg:portrait:min-h-[1024px] max-lg:portrait:min-w-[768px] max-lg:portrait:max-w-[800px] max-lg:landscape:h-[768px] max-lg:landscape:w-[1024px]">
       <Navbar>
         <Navbar.Logo src={logo} />
         <Navbar.Items>
           <Navbar.Item className="text-xl">工廠日誌</Navbar.Item>
         </Navbar.Items>
       </Navbar>
-      <div className="flex h-full overflow-auto bg-zinc-100">
+      <div className="relative flex h-full overflow-auto bg-zinc-100">
         <Sidebar />
+        <div className="absolute right-8 top-4 flex w-full justify-end gap-3 text-xs">
+          <button
+            type="button"
+            aria-description="change language to traditional chinese"
+            className="z-50 rounded bg-stone-200 p-2 shadow-sm shadow-gray-400"
+            onClick={() => changeLanguage("zh-TW")}
+          >
+            {t("繁體")}
+          </button>
+          <button
+            type="button"
+            aria-description="change language to english"
+            className="z-50 rounded bg-stone-200 p-2 shadow-sm shadow-gray-400"
+            onClick={() => changeLanguage("en")}
+          >
+            {t("英文")}
+          </button>
+          <button
+            type="button"
+            aria-description="change language to simplified chinese"
+            className="z-50 rounded bg-stone-200 p-2 shadow-sm shadow-gray-400"
+            onClick={() => changeLanguage("zh-CN")}
+          >
+            {t("簡體")}
+          </button>
+        </div>
         <div className="relative flex-grow border-4 pt-4">
-          <div className="absolute right-8 top-8 flex w-full justify-end gap-3 text-xs">
-            <button
-              type="button"
-              aria-description="change language to traditional chinese"
-              className="rounded bg-stone-200 p-2 shadow-sm shadow-gray-400"
-              onClick={() => changeLanguage("zh-TW")}
-            >
-              {t("繁體")}
-            </button>
-            <button
-              type="button"
-              aria-description="change language to english"
-              className="rounded bg-stone-200 p-2 shadow-sm shadow-gray-400"
-              onClick={() => changeLanguage("en")}
-            >
-              {t("英文")}
-            </button>
-            <button
-              type="button"
-              aria-description="change language to simplified chinese"
-              className="rounded bg-stone-200 p-2 shadow-sm shadow-gray-400"
-              onClick={() => changeLanguage("zh-CN")}
-            >
-              {t("簡體")}
-            </button>
-          </div>
           <Outlet />
         </div>
       </div>
