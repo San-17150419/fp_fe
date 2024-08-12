8fd0bfa67b6cee0b733a8492eab698d56386236f
ZENG-JING-YUAN <steven.tseng@heavypower.com.tw>
2024-08-12 16:20:13 +0800

    feat: enhance loading component
    - Added translation support to the `Loading` component, enabling the
    "Loading..." text to be localized using the `useTranslation` hook.
    - Improved flexibility by introducing an optional `children` prop to
    allow additional content or messages to be displayed within the loading
    screen.
    - Enhanced the visual appearance by adding a blurred backdrop and
    semi-transparent background for better emphasis during loading states.

--- src/Components/Loading.tsx
+++ src/Components/Loading.tsx
@@ -1,10 +1,17 @@
 import { VscLoading } from "react-icons/vsc";
-export default function Loading() {
+import { useTranslation } from "react-i18next";
+
+type LoadingProps = {
+  children?: React.ReactNode;
+};
+export default function Loading({ children }: LoadingProps) {
+  const { t } = useTranslation();
   return (
-    <div className="fixed inset-0 z-50 flex h-full flex-col items-center justify-center gap-5 text-3xl">
-      <p className="loading">Loading ...</p>
+    <div className="fixed inset-0 z-50 flex backdrop-blur-sm h-full flex-col items-center justify-center gap-5 bg-gray-200/50 text-3xl">
+      <p className="loading">{t("Loading")}...</p>
 
       <VscLoading className="animate-spin text-5xl" />
+      {children}
     </div>
   );
 }
