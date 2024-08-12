af55c4aaa302d311d06a7d1988f5492748281643
ZENG-JING-YUAN <steven.tseng@heavypower.com.tw>
2024-08-12 16:06:55 +0800

    feat: integrate authentication and improve UI
    - Integrated authentication by adding username display and
    login/logout button to the navbar
    - Added conditional rendering to display the login button when the user
    is not authenticated and a logout button when they are.
    - Improved layout so that sidebar won't overlap with button group.
    - Removed commented-out sections and unused elements

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import Sidebar from "./Components/modd/Sidebar/Sidebar";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate, useLocation } from "react-router-dom";
 import logo from "./assets/logo_banner5.png";
 import Navbar from "./Components/modd/Navbar/Navbar";
 import { useTranslation } from "react-i18next";
-import React, { useRef, useState, useEffect, Suspense } from "react";
+import { Suspense } from "react";
 import Loading from "./Components/Loading";
+import { useAuthContext } from "./Components/modd/Table/FactoryLog/AuthContext";
 
 function App() {
   const { t, i18n } = useTranslation();
   const changeLanguage = (lng: string) => {
     i18n.changeLanguage(lng);
   };
+  const location = useLocation();
+  const { token, username, setToken, logout, login } = useAuthContext();
+  const navigate = useNavigate();
   return (
     <div className="mx-auto flex flex-col max-2xl:h-full lg:w-full desktop:min-h-[768px] desktop:min-w-[1366px] xl:max-h-[1080px] xl:max-w-[1920px] 2xl:h-[1080px] max-lg:portrait:max-h-[1280px] max-lg:portrait:min-h-[1024px] max-lg:portrait:min-w-[768px] max-lg:portrait:max-w-[800px] max-lg:landscape:h-[768px] max-lg:landscape:w-[1024px]">
       <Navbar>
@@ -19,44 +23,55 @@ function App() {
           <Navbar.Item className="text-xl">工廠日誌</Navbar.Item>
         </Navbar.Items>
       </Navbar>
-      <div className="relative flex h-full overflow-auto bg-zinc-100">
+      <div className="flex h-full overflow-auto bg-zinc-100">
         <Sidebar />
-        <div className="absolute right-8 top-4 flex w-full justify-end gap-3 text-xs">
-          <button
-            type="button"
-            aria-description="change language to traditional chinese"
-            className="z-50 rounded bg-stone-200 p-2 shadow-sm shadow-gray-400"
-            onClick={() => changeLanguage("zh-TW")}
-          >
-            {t("繁體")}
-          </button>
-          <button
-            type="button"
-            aria-description="change language to english"
-            className="z-50 rounded bg-stone-200 p-2 shadow-sm shadow-gray-400"
-            onClick={() => changeLanguage("en")}
-          >
-            {t("英文")}
-          </button>
-          <button
-            type="button"
-            aria-description="change language to simplified chinese"
-            className="z-50 rounded bg-stone-200 p-2 shadow-sm shadow-gray-400"
-            onClick={() => changeLanguage("zh-CN")}
-          >
-            {t("簡體")}
-          </button>
-        </div>
-        <div className="relative flex-grow border-4 pt-4">
+        <div className="relative w-full overflow-hidden border">
+          <div className="absolute left-10 top-4 w-full text-xs ">
+            <h2 className="my-auto text-xl underline">{username}</h2>
+            {location.pathname !== "/login" && (
+              <button
+                type="button"
+                className="z-10 rounded bg-stone-200 p-2 shadow-sm shadow-gray-400"
+                // className="z-5 rounded bg-stone-200 p-2 shadow-sm shadow-gray-400"
+                onClick={() => (token === null ? navigate("/login") : logout())}
+              >
+                {token === null ? "登入" : "登出"}
+              </button>
+            )}
+          </div>
+          <div className="absolute right-10 top-4 flex gap-4 text-xs">
+            <button
+              type="button"
+              aria-description="change language to traditional chinese"
+              className="z-50 rounded bg-stone-200 p-2 shadow-sm shadow-gray-400"
+              onClick={() => changeLanguage("zh-TW")}
+            >
+              {t("繁體")}
+            </button>
+            <button
+              type="button"
+              aria-description="change language to english"
+              className="z-50 rounded bg-stone-200 p-2 shadow-sm shadow-gray-400"
+              onClick={() => changeLanguage("en")}
+            >
+              {t("英文")}
+            </button>
+            <button
+              type="button"
+              aria-description="change language to simplified chinese"
+              className="z-50 rounded bg-stone-200 p-2 shadow-sm shadow-gray-400"
+              onClick={() => changeLanguage("zh-CN")}
+            >
+              {t("簡體")}
+            </button>
+          </div>
+          {/* <main className="bg-black"> */}
           <Suspense fallback={<Loading />}>
             <Outlet />
           </Suspense>
+          {/* </main> */}
         </div>
       </div>
-
-      {/* <div className="absolute bottom-[50%] left-[50%] p-4 text-5xl">
-        Width: {size.width}px, Height: {size.height}px
-      </div> */}
     </div>
   );
 }
