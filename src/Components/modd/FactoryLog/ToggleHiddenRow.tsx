bf27f8d3a17c0c547cd0d871bf3d2870f24802cb
ZENG-JING-YUAN <steven.tseng@heavypower.com.tw>
2024-08-15 17:07:46 +0800

    fix: Align clickable areas with visual elements
    - Improved the alignment of clickable areas with their visual
    counterparts in the `HiddenRowsToggle` component.
    - Updated the structure of `li` and `label` elements to ensure that the
    entire visible area is clickable, enhancing the user experience.
    - Adjusted styling to ensure that checkbox labels and surrounding areas
    respond accurately to user interactions.
    - Updated `useCloseAndEscape` hook configuration to ensure focus state
    return to the correct button after dropdown is closed.

--- src/Components/modd/FactoryLog/ToggleHiddenRow.tsx
+++ src/Components/modd/FactoryLog/ToggleHiddenRow.tsx
@@ -1,6 +1,7 @@
 import { useState, useRef } from "react";
 import useCloseAndEscape from "../../../hooks/useCloseAndEscape";
 import clsx from "clsx";
+import { useTheme } from "../../../stores/ThemeContext";
 
 type HiddenRowsToggleProps = {
   hiddenRows: string[];
@@ -21,16 +22,23 @@ const HiddenRowsToggle = ({
   toggleAllVisibility,
   position = "bottom-left",
 }: HiddenRowsToggleProps) => {
-  const dropdownRef = useRef(null);
+  const dropdownRef = useRef<HTMLDivElement>(null);
   const [open, setOpen] = useState(false);
-
-  useCloseAndEscape(dropdownRef, () => setOpen(false), open);
+  const { isSemiBold, isTextBase } = useTheme();
+  useCloseAndEscape(dropdownRef, () => setOpen(false), open, false);
 
   return (
-    <div ref={dropdownRef} className="relative mt-2 w-full max-w-fit">
+    <div
+      className="relative mt-2 w-full max-w-fit bg-white p-3"
+      ref={dropdownRef}
+    >
       <button
         type="button"
-        className="block rounded-md bg-gray-200 px-2 py-1 text-base shadow shadow-slate-400 hover:bg-gray-300"
+        className={clsx(
+          "block rounded-md bg-gray-200 px-2 py-1 shadow shadow-slate-400 hover:bg-gray-300",
+          isSemiBold ? "font-semibold" : "font-normal",
+          isTextBase ? "text-base" : "text-sm",
+        )}
         onClick={() => setOpen((prev) => !prev)}
       >
         顯示隱藏系列
@@ -41,40 +49,53 @@ const HiddenRowsToggle = ({
           className={clsx(
             "absolute z-50 flex w-fit flex-col rounded-md bg-white shadow shadow-slate-400",
             {
-              "right-0 top-full mt-2": position === "bottom-right",
-              "left-0 top-full mt-2": position === "bottom-left",
-              "bottom-full right-0 mb-2": position === "top-right",
-              "bottom-full left-0 mb-2": position === "top-left",
+              "right-1 top-full ": position === "bottom-right",
+              "left-3 top-full ": position === "bottom-left",
+              "bottom-full right-1 ": position === "top-right",
+              "bottom-full left-1 ": position === "top-left",
             },
           )}
         >
-          <li className="flex gap-2 rounded-md py-2 pl-3 pr-4 hover:bg-gray-100">
-            <input
-              type="checkbox"
-              checked={allHiddenToggled}
-              id="toggle-all"
-              onChange={toggleAllVisibility}
-            />
-            <label className="w-full text-left text-base" htmlFor="toggle-all">
-              {!allHiddenToggled ? "Show all" : "Hide all"}
+          <li className="flex gap-2 rounded-md hover:bg-gray-100">
+            <label
+              className={clsx(
+                "mt-1 w-full",
+                isSemiBold ? "font-semibold" : "font-normal",
+                isTextBase ? "text-base" : "text-sm",
+              )}
+              htmlFor="toggle-all"
+            >
+              <span className="mx-3 flex w-full gap-2">
+                <input
+                  type="checkbox"
+                  checked={allHiddenToggled}
+                  id="toggle-all"
+                  onChange={toggleAllVisibility}
+                  aria-label="Toggle all hiddenRows"
+                />
+                {!allHiddenToggled ? "Show all" : "Hide all"}
+              </span>
             </label>
           </li>
           {hiddenRows.map((row, index) => (
-            <li
-              className="flex gap-2 rounded-md py-2 pl-3 pr-4 hover:bg-gray-100"
-              key={index}
-            >
-              <input
-                type="checkbox"
-                id={`${row}-${index}`}
-                checked={visibleRows.includes(row)}
-                onChange={() => toggleVisibility(row)}
-              />
+            <li key={index} className="rounded-md hover:bg-gray-100">
               <label
-                className="w-full text-left text-base"
+                className={clsx(
+                  "mt-1 w-full",
+                  isSemiBold ? "font-semibold" : "font-normal",
+                  isTextBase ? "text-base" : "text-sm",
+                )}
                 htmlFor={`${row}-${index}`}
               >
-                {row}
+                <span className="mx-3 flex w-full gap-2">
+                  <input
+                    type="checkbox"
+                    id={`${row}-${index}`}
+                    checked={visibleRows.includes(row)}
+                    onChange={() => toggleVisibility(row)}
+                  />
+                  {row}
+                </span>
               </label>
             </li>
           ))}
