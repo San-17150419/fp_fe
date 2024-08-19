d9ac232049728c1373eb015fc085fa532ce19055
ZENG-JING-YUAN <steven.tseng@heavypower.com.tw>
2024-08-19 15:52:46 +0800

    chore: Comment out unused code

--- src/pages/Test.tsx
+++ src/pages/Test.tsx
@@ -1,21 +1,10 @@
-import {
-  Document,
-  Page,
-  Text,
-  View,
-  StyleSheet,
-  pdf,
-} from "@react-pdf/renderer";
+
 import { useState } from "react";
-import { saveAs } from "file-saver";
+import Modal from "../Components/modd/Modal/NonDialogModal";
 export default function Chart() {
   const [file, setFile] = useState<FileList | null>(null);
   const [previewImgUrls, setPreviewImgUrls] = useState<string[]>([]);
-  const ref = useRef<HTMLInputElement>(null);
-  const imgRef = useRef<HTMLImageElement>(null);
-  const herfRef = useRef<HTMLAnchorElement>(null);
-
-
+  const [showModal, setShowModal] = useState(false);
 
   function createCSV() {
     const rows = [
@@ -35,15 +24,15 @@ export default function Chart() {
     return csvContent;
   }
   createCSV();
-  function downloadCSV(csvContent: string) {
-    const encodedUri = encodeURI(csvContent);
-    const link = document.createElement("a");
-    link.setAttribute("href", encodedUri);
-    link.setAttribute("download", "data.csv");
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-  }
+  // function downloadCSV(csvContent: string) {
+  //   const encodedUri = encodeURI(csvContent);
+  //   const link = document.createElement("a");
+  //   link.setAttribute("href", encodedUri);
+  //   link.setAttribute("download", "data.csv");
+  //   document.body.appendChild(link);
+  //   link.click();
+  //   document.body.removeChild(link);
+  // }
 
   // const csvContent = createCSV();
   // downloadCSV(csvContent);
@@ -53,27 +42,17 @@ export default function Chart() {
     console.log(event.target.files);
 
     if (event.target.files) {
-      // const filesNumber = event.target.files.length;
-      // for (let i = 0; i < filesNumber; i++) {
-      //   try {
-      //     const file = event.target.files[i];
-      //     const url = await fileToDataString(file);
-      //     setPreviewImgUrls([...previewImgUrls, url]);
-      //   } catch (error) {
-      //     console.log(error);
-      //   }
-      // }
       try {
-        const file = event.target.files[0];
-        const url = await fileToDataString(file);
-        setPreviewImgUrls([...previewImgUrls, url]);
+        for (const file of event.target.files) {
+          const url = await fileToDataString(file);
+          setPreviewImgUrls((prev) => [...prev, url]);
+        }
+        setFile(event.target.files);
       } catch (error) {
         console.log(error);
       }
     }
   };
-  const test = URL;
-  const test2 = window.URL;
 
   const fileToDataString = (file: File) => {
     return new Promise<string>((resolve, reject) => {
@@ -87,20 +66,6 @@ export default function Chart() {
       };
     });
   };
-  const styles = StyleSheet.create({
-    page: {
-      backgroundColor: "white",
-      color: "white",
-    },
-    section: {
-      margin: 10,
-      padding: 10,
-    },
-    viewer: {
-      width: window.innerWidth, //the pdf viewer will take up all of the width and height
-      height: window.innerHeight,
-    },
-  });
   function returnFileSize(number: number) {
     if (number < 1e3) {
       return `${number} bytes`;
@@ -111,272 +76,74 @@ export default function Chart() {
     }
   }
 
-  const downloadPDF = async () => {
-    const fileName = "test.pdf";
-    const blob = await pdf(
-      <Document>
-        {/*render a single page*/}
-        <Page size="A4" orientation="landscape" style={styles.page}>
-          <View
-            style={{
-              display: "flex",
-              borderTop: "1px solid black",
-              flexDirection: "row",
-              flexGrow: 1,
-              justifyContent: "space-between",
-              alignItems: "center",
-            }}
-          >
-            <Text
-              style={{
-                color: "red",
-                border: "1px solid black",
-                flexGrow: 1,
-                alignContent: "center",
-              }}
-            >
-              213
-            </Text>
-            <Text
-              style={{
-                color: "red",
-                border: "1px solid black",
-                flexGrow: 1,
-                alignContent: "center",
-                alignItems: "center",
-                justifyContent: "center",
-                textAlign: "center",
-              }}
-            >
-              213
-            </Text>
-            <Text
-              style={{
-                color: "red",
-                border: "1px solid black",
-                flexGrow: 1,
-                alignContent: "center",
-              }}
-            >
-              213
-            </Text>
-            <Text
-              style={{
-                color: "red",
-                border: "1px solid black",
-                flexGrow: 1,
-                alignContent: "center",
-              }}
-            >
-              213
-            </Text>
-            <Text
-              style={{
-                color: "red",
-                border: "1px solid black",
-                flexGrow: 1,
-                alignContent: "center",
-              }}
-            >
-              213
-            </Text>
-          </View>
-          <View style={styles.section}>
-            <Text>World</Text>
-          </View>
-        </Page>
-        <Page size="A4" orientation="landscape" style={styles.page}>
-          <View
-            style={{
-              display: "flex",
-              borderTop: "1px solid black",
-              flexDirection: "row",
-              flexGrow: 1,
-              justifyContent: "space-between",
-              alignItems: "center",
-            }}
-          >
-            <Text
-              style={{
-                color: "red",
-                border: "1px solid black",
-                flexGrow: 1,
-                alignContent: "center",
-              }}
-            >
-              213
-            </Text>
-            <Text
-              style={{
-                color: "red",
-                border: "1px solid black",
-                flexGrow: 1,
-                alignContent: "center",
-                alignItems: "center",
-                justifyContent: "center",
-                textAlign: "center",
-              }}
-            >
-              213
-            </Text>
-            <Text
-              style={{
-                color: "red",
-                border: "1px solid black",
-                flexGrow: 1,
-                alignContent: "center",
-              }}
-            >
-              213
-            </Text>
-            <Text
-              style={{
-                color: "red",
-                border: "1px solid black",
-                flexGrow: 1,
-                alignContent: "center",
-              }}
-            >
-              213
-            </Text>
-            <Text
-              style={{
-                color: "red",
-                border: "1px solid black",
-                flexGrow: 1,
-                alignContent: "center",
-              }}
-            >
-              213
-            </Text>
-          </View>
-          <View style={styles.section}>
-            <Text>World</Text>
-          </View>
-        </Page>
-      </Document>,
-    ).toBlob();
-    saveAs(blob, fileName);
-  };
   return (
     <>
-      {/* <BubbleChart /> */}
-      <form action="" className="flex flex-col gap-4 border border-black">
-        <label htmlFor="file">User Upload File</label>
+      <Modal isOpen={showModal} onClose={() => setShowModal(false)}></Modal>
+      <form
+        action=""
+        className="mx-20 mt-10 flex flex-col gap-4 border border-black p-2"
+      >
+        <label htmlFor="file" hidden>
+          User Upload File
+        </label>
         <input
           type="file"
           name="file"
           id="file"
+          title="import csv file"
           accept="image/*"
           onChange={handleFileChange}
           multiple
         />
+
+        <div>
+          {file &&
+            Array.from(file).map((file, index) => (
+              <>
+                <div className="flex gap-2" key={file.name}>
+                  {file.name} - {returnFileSize(file.size)}
+                  <img
+                    src={URL.createObjectURL(file)}
+                    alt="preview"
+                    className="w-40 object-scale-down"
+                    // className="w-40 object-contain"
+                    // className="w-5 object-contain"
+                  />
+                  <a
+                    className="ml-2 text-blue-400 underline"
+                    href={URL.createObjectURL(file)}
+                  >
+                    preview {index}
+                  </a>
+                </div>
+              </>
+            ))}
+        </div>
       </form>
-      <div>
-        {file &&
-          Array.from(file).map((file) => (
-            <div key={file.name}>
-              {file.name} - {returnFileSize(file.size)}
-            </div>
-          ))}
-      </div>
-      <div>
+
+      <div className="w-40">
         {previewImgUrls.map((url, index) => (
           <>
-            <img alt="" src={url} key={url.slice(0, 10) + index} />
-
-            {/* <a href={url} download={file?.[index].name}>
-              {file?.[index].name}
-            </a> */}
+            <img
+              alt=""
+              src={url}
+              className="object-contain"
+              key={url.slice(0, 10) + index}
+            />
           </>
         ))}
       </div>
-      <button
+      {/* <button
         type="button"
         onClick={() => downloadCSV(createCSV())}
         className="border border-black"
       >
         download
       </button>
-      <PDFViewer
-        style={styles.viewer}
-        // showToolbar={false}
-      >
-        {/* Start of the document*/}
-        <Document>
-          {/*render a single page*/}
-          <Page size="A4" orientation="landscape" style={styles.page}>
-            <View
-              style={{
-                display: "flex",
-                borderTop: "1px solid black",
-                flexDirection: "row",
-                flexGrow: 1,
-                justifyContent: "space-between",
-                alignItems: "center",
-              }}
-            >
-              <Text
-                style={{
-                  color: "red",
-                  border: "1px solid black",
-                  flexGrow: 1,
-                  alignContent: "center",
-                }}
-              >
-                213
-              </Text>
-              <Text
-                style={{
-                  color: "red",
-                  border: "1px solid black",
-                  flexGrow: 1,
-                  alignContent: "center",
-                  alignItems: "center",
-                  justifyContent: "center",
-                  textAlign: "center",
-                }}
-              >
-                213
-              </Text>
-              <Text
-                style={{
-                  color: "red",
-                  border: "1px solid black",
-                  flexGrow: 1,
-                  alignContent: "center",
-                }}
-              >
-                213
-              </Text>
-              <Text
-                style={{
-                  color: "red",
-                  border: "1px solid black",
-                  flexGrow: 1,
-                  alignContent: "center",
-                }}
-              >
-                213
-              </Text>
-              <Text
-                style={{
-                  color: "red",
-                  border: "1px solid black",
-                  flexGrow: 1,
-                  alignContent: "center",
-                }}
-              >
-                213
-              </Text>
-            </View>
-          </Page>
-        </Document>
-      </PDFViewer>
-      ,
+
       <div className="outline">
         <button onClick={downloadPDF}>download pdf</button>
-      </div>
+      </div> */}
     </>
   );
 }
