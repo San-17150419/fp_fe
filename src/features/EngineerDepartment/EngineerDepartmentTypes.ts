2016ffddb2c4e48ab58b68746eca7d78439c5b5e
ZENG-JING-YUAN <steven.tseng@heavypower.com.tw>
2024-08-21 16:32:16 +0800

    feature: Add ENGDepartmentFilterData type

--- src/features/EngineerDepartment/EngineerDepartmentTypes.ts
+++ src/features/EngineerDepartment/EngineerDepartmentTypes.ts
@@ -13,6 +13,37 @@ export type Maker = ENGDepartmentPreData["preData"]["maker"][number];
 
 export type Site = "GD" | "HP" | "DL" | "D08" | "停用";
 
+export type ENGDepartmentFilterData = {
+  post: {
+    sys: string;
+    sn_num: string;
+    prod_name_board: string;
+    mold_num: string;
+    property: string;
+    site: Site;
+  };
+  data: {
+    sn_num: string;
+    site: Site;
+    state: Status;
+    sys: string;
+    property: string;
+    brand: number;
+    prod_name_board: string;
+    pnb_state: "incomplete" | "done";
+    prod_name_nocolor: string;
+    mold_num: string;
+    hole_num: number;
+    block_num: number;
+    property_num: string;
+    maker: string;
+    dutydate_month: string;
+    spare: string;
+    id_ms: number;
+    dutydate_last: string;
+  }[];
+};
+
 type Status =
   | "開發中"
   | "製模中"
