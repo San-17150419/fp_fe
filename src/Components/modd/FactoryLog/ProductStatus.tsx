0791b7988c6c09c17cb5df15f31458b56fea5996
ZENG-JING-YUAN <steven.tseng@heavypower.com.tw>
2024-08-28 17:11:06 +0800

    fix: Fix state management in ProductStatus - Fixed a bug where states in `ProductStatus` is not reset correctly when value remain the same and point changes.
    style is not apply correctly right now.

--- src/Components/modd/FactoryLog/ProductStatus.tsx
+++ src/Components/modd/FactoryLog/ProductStatus.tsx
@@ -7,7 +7,10 @@ type ProductStatusProps = {
   point: "ar" | "pamt_h";
 };
 
-const ProductStatus: React.FC<ProductStatusProps> = ({ value, point }) => {
+const ProductStatus: React.FC<ProductStatusProps> = ({
+  value,
+  point,
+}) => {
   const { t } = useTranslation();
   const [isLowest, setIsLowest] = useState(false);
   const [isLowerThanStandard, setIsLowerThanStandard] = useState(false);
@@ -19,69 +22,67 @@ const ProductStatus: React.FC<ProductStatusProps> = ({ value, point }) => {
     numberOfConsecutiveLowerThanStandard,
     setNumberOfConsecutiveLowerThanStandard,
   ] = useState(0);
-  // const { isSemiBold, isTextBase } = useTheme();
+
   useEffect(() => {
-    function findLastNonZeroValue(data: number[]) {
-      for (let i = data.length - 1; i >= 0; i--) {
-        if (data[i] !== 0) {
-          return { value: data[i], index: i };
-        }
-      }
-      return { value: 0, index: -1 };
-    }
+    setIsLowest(false);
+    setIsLowerThanStandard(false);
+    setIsLowerThanStandardConsecutively(false);
+    setNumberOfConsecutiveLowerThanStandard(0);
+
     const calculateStatus = (data: number[]) => {
-      const { value: current, index: startIndex } = findLastNonZeroValue(data);
-      let consecutiveCount = 0;
-      // Technically, when point is pamt_h, I don't need this check 
-      // Check for consecutive values lower than standard (85)
-      for (let i = startIndex; i >= 0; i--) {
-        //
-        if (data[i] < 0.85) {
-          consecutiveCount++;
-        } else {
-          break;
-        }
+      // Filter out zero values as they represent missing data
+      const nonZeroNumbers = data.filter((value) => value !== 0);
+
+      // If there are no non-zero values, early return with default state updates
+      if (nonZeroNumbers.length === 0) {
+        setNumberOfConsecutiveLowerThanStandard(0);
+        setIsLowerThanStandardConsecutively(false);
+        setIsLowest(false);
+        setIsLowerThanStandard(false);
+        return;
       }
 
+      // Find the lowest and the last non-zero value
+      const lowest = Math.min(...nonZeroNumbers);
+      const current = nonZeroNumbers[nonZeroNumbers.length - 1];
+
+      // Count consecutive values lower than the standard (0.85)
+      const consecutiveCount = nonZeroNumbers.reduceRight((count, value) => {
+        return value < 0.85 ? count + 1 : 0;
+      }, 0);
+
+      // Update state based on the calculations
       setNumberOfConsecutiveLowerThanStandard(consecutiveCount);
       setIsLowerThanStandardConsecutively(consecutiveCount > 1);
-
-      // Filter out 0 values (0 means no data) and find the lowest number
-      const nonZeroNumbers = data.filter((v) => v !== 0);
-      const lowest = Math.min(...nonZeroNumbers);
-      if (current === lowest) {
-        if (current < 0.85) {
-          setIsLowerThanStandard(true);
-        }
-        setIsLowest(current === lowest);
-      }
+      setIsLowest(current === lowest);
+      setIsLowerThanStandard(current < 0.85 && current === lowest);
     };
 
     calculateStatus(value);
   }, [value]);
 
+  const getStatusClassNames = (point: string) => {
+    const baseClassNames =
+      "rounded-full px-2 py-1 text-sm font-semibold shadow";
+
+    if (point === "ar") {
+      return isLowerThanStandard
+        ? `bg-amber-500 text-white shadow-amber-700 ${baseClassNames}`
+        : `bg-green-500 shadow-green-700 ${baseClassNames}`;
+    }
+
+    return `bg-red-500 text-white shadow-red-700 ${baseClassNames}`;
+  };
+
   return (
     <div className="flex flex-wrap justify-center gap-2">
       {isLowest && (
-        <p
-          className={clsx(
-            "rounded-full px-2 py-1 text-sm font-semibold shadow",
-            point === "ar" && isLowerThanStandard
-              ? "bg-amber-500 text-white shadow-amber-700"
-              : "bg-green-500 shadow-green-700",
-            point === "pamt_h" && "bg-red-500 text-white shadow-red-700",
-          )}
-        >
+        <p className={getStatusClassNames(point)}>
           {point === "pamt_h" ? t("最低機均產出") : t("最低達成率")}
         </p>
       )}
       {point === "ar" && isLowerThanStandardConsecutively && (
-        <p
-          className={clsx(
-            "rounded-full bg-red-500 px-2 py-1 text-sm font-semibold text-white shadow shadow-red-700",
-            // isTextBase ? "text-base" : "text-sm",
-          )}
-        >
+        <p className={getStatusClassNames(point)}>
           {t("連續低於標準")}
           {numberOfConsecutiveLowerThanStandard > 1 &&
             ` (${numberOfConsecutiveLowerThanStandard})`}
