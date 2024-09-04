e5596c60f2cdca0a2969e250d0b2b1035c317b2b
ZENG-JING-YUAN <steven.tseng@heavypower.com.tw>
2024-09-04 16:18:04 +0800

    feat: add disabled state to Select component
    - Allow `Select` component to be disabled.

--- src/Components/modd/Select/Select.tsx
+++ src/Components/modd/Select/Select.tsx
@@ -10,14 +10,19 @@ import { useTranslation } from "react-i18next";
 import cn from "../../../util/cn";
 import clsx from "clsx";
 
+// TODO: Make is controlled component. I should be able to reset the value from outside
+
 export default function Select({
   options,
   className,
   onSelect,
   name,
   defaultValue,
+  disabled = false,
+  // value = options[0],
 }: SelectProps) {
   const { t } = useTranslation();
+
   // If user wish to add a placeholder, add it to options.
   const [selectedOption, setSelectedOption] = useState<Option>(
     defaultValue || options[0],
@@ -29,11 +34,11 @@ export default function Select({
     }
     setSelectedOption(option);
   };
-
   return (
     <Listbox
       value={selectedOption}
       onChange={handleSelect}
+      disabled={disabled}
       defaultValue={defaultValue}
       name={name}
       as="div"
@@ -44,8 +49,11 @@ export default function Select({
           <ListboxButton
             id={name}
             className={clsx(
-              "box-border flex w-full items-center overflow-clip text-nowrap rounded-md border border-gray-300 bg-white px-2 py-1 text-left shadow shadow-slate-300 hover:border-sky-300 focus:border-2 focus:border-sky-400 desktop:px-3 desktop:py-4",
+              "box-border flex w-full items-center overflow-clip text-nowrap rounded-md border border-gray-300 bg-white px-2 py-1 text-left shadow shadow-slate-300 desktop:px-3 desktop:py-4",
               open ? "border-blue-300" : "border-gray-300",
+              disabled && "opacity-50",
+              !disabled &&
+                "cursor-pointer hover:border-sky-300 focus:border-2 focus:border-sky-400",
             )}
           >
             <p className="max-w-4/5 truncate">{t(selectedOption.text)}</p>
@@ -83,8 +91,10 @@ export type Option = {
 };
 type SelectProps = {
   options: Option[];
-  name: string;
+  name?: string;
   className?: string;
   onSelect?: (option: Option) => void;
   defaultValue?: Option;
+  disabled?: boolean;
+  value?: Option;
 };
