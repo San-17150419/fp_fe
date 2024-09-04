6c96ff5b3803034fc819c0a9a0ba3fc6456b3619
ZENG-JING-YUAN <steven.tseng@heavypower.com.tw>
2024-09-04 16:21:46 +0800

    feat: add hook for fetching ENG deparment pre-data
    - Created `useENGDepartmentPreData` hook to fetch pre-data for the
    Engineer Department using React Query. The hook fetches data from the c
    onfigured API, processes it into filter options using
    `createEngineerFilterOptions`, and returns the data, error, and loading
    state. Added type definitions for the returned filter options and
    pre-data.
    
    Might remove `ENGDepartmentContext` later.

---
+++ src/features/EngineerDepartment/hooks/useENGDepartmentPreData.ts
@@ -0,0 +1,40 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { type PreData } from "../types";
+import { type Option } from "../../../Components/modd/Select/Select";
+const api = import.meta.env.VITE_ENGINEER_DEPARTMENT_URL + "pre-data/";
+import { createEngineerFilterOptions } from "../utils/generateOptions";
+
+export default function useENGDepartmentPreData() {
+  const { error, data, isPending } = useQuery<PreFilterData>({
+    queryKey: ["ENGDPreData"],
+    queryFn: async () => {
+      const response = await axios.get<PreData>(api);
+      return {
+        states: response.data,
+        ...createEngineerFilterOptions(response.data.preData),
+      };
+    },
+  });
+
+  if (isPending || !data) return { isPending: true, error: null, data: null };
+  if (error) return { isPending: false, error, data: null };
+
+  return { isPending: false, error: null, data };
+
+  //   return {
+  //     isPending,
+  //     isLoading,
+  //     error,
+  //     data: data,
+  //   };
+}
+
+export type PreFilterData = {
+  makerOptions: Option[];
+  seriesOptions: Option[];
+  siteOptions: Option[];
+  propertyOptions: Option[];
+  statusOptions: Option[];
+  states: PreData;
+};
