9fe90339dbf65c31d0e9a897cf83b6a81e8b410b
ZENG-JING-YUAN <steven.tseng@heavypower.com.tw>
2024-09-05 16:40:35 +0800

    refactor: Improve Event component UX and layout
    - Added a TODO to rename the component to a more descriptive name for
    clarity.
    - Identified a potential performance issue where certain tables take
    longer to render, and added TODOs for:
      1. Providing a visual cue to inform users that data is being fetched.
      2. Investigating whether the delay is due to data fetching time or
      other reasons.
    - Adjusted modal layout by setting a minimum width for better
    readability and reducing the maximum height to improve table
    overflow handling.
    - Noted a TODO to address janky modal animations, especially for
    the `D08` site.

--- src/features/EngineerDepartment/Event.tsx
+++ src/features/EngineerDepartment/Event.tsx
@@ -10,6 +10,9 @@ type EventProps = {
   dutydate_last: string;
 };
 
+// TODO: Rename this component to something more descriptive.
+// TODO: Data is fetched upon demand. So data is not always ready when modal is fully rendered. The issue is I am not sure why certain table takes longer to render. Even if other table has significantly more data. 1. Add visual cue to inform users data is being fetched. 2. Check the time for table to be ready is caused by the time it takes to fetch data or something else. 
+
 export default function Event({ sn_num, site, dutydate_last }: EventProps) {
   const [open, setOpen] = useState(false);
   const [postData, setPostData] = useState<PostData | null>(null);
@@ -40,11 +43,12 @@ export default function Event({ sn_num, site, dutydate_last }: EventProps) {
         {dutydate_last}
       </div>
       {open && (
+        // TODO: animation is a bit janky. Especially when site is D08. 
         <Modal isOpen={open} onClose={() => setOpen(false)}>
           {/* <p>{JSON.stringify(postData, null, 2)}</p> */}
           {postData && (
-            <div className="mx-auto w-full px-1 py-4">
-              <div className="relative mx-auto max-h-[80vh] max-w-full overflow-auto px-2">
+            <div className="mx-auto min-w-[50vw] px-1 py-4">
+              <div className="relative mx-auto max-h-[70vh] max-w-full overflow-auto px-2">
                 {/* <div className="mx-auto max-w-[90%] max-h-[80vh] bg-red-200 overflow-auto"> */}
                 <table className="h-full w-full table-auto border-separate border-spacing-0">
                   <thead className="sticky top-0 z-10 backdrop-opacity-0">
