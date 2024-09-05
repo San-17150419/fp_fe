70a57e5dd38c099d13db93a3f93296e152427401
ZENG-JING-YUAN <steven.tseng@heavypower.com.tw>
2024-09-05 16:36:56 +0800

    feat: Enhance form
    - Added a regular expression for validating `moldNum` input to ensure
    it matches the expected format.
    - Introduced an `isMoldNumValid` state to track the validity of
    `moldNum` and provide immediate feedback to users.
    - Updated the `onBlur` event to ensure the request for a new serial
    number (`sn_num`) is only triggered when the input is valid.
    - Temporarily disabled and commented out `useCreateMold` and related
    code while testing.
    - Filtered `seriesOptions` to exclude "模仁" from the dropdown options.
    
    These updates are in progress and part of the testing phase to
    improve input validation and form submission behavior.

--- src/features/EngineerDepartment/CreateMold.tsx
+++ src/features/EngineerDepartment/CreateMold.tsx
@@ -1,5 +1,5 @@
 import { useState, type FormEvent, useEffect } from "react";
-import useCreateMold from "./hooks/useCreateMold";
+// import useCreateMold from "./hooks/useCreateMold";
 import Modal from "../../Components/modd/Modal/NonDialogModal";
 import { type PreFilterData } from "./hooks/useENGDepartmentPreData";
 import withPreData from "./WithPreData";
@@ -21,18 +21,26 @@ export type SelectConfig = {
   options: Array<Option>;
 };
 
-export default function CreateMold({
-  makerOptions,
-  propertyOptions,
-  siteOptions,
-  seriesOptions,
-}: PreFilterData) {
+const regexforMoldNumInput = /^[A-Za-z]$|^A1$/;
+
+export default function CreateMold({ seriesOptions }: PreFilterData) {
   const [showModal, setShowModal] = useState(false);
-  const { handleCreateNewMold } = useCreateMold();
+  // const { handleCreateNewMold } = useCreateMold();
   const [moldNum, setMoldNum] = useState<string>("");
   const [sys, setSys] = useState<Sys>("");
   const [sn_num, setSnNum] = useState<string>("");
   const [isSnNumReady, setIsSnNumReady] = useState<boolean>(false);
+  // I thihk it make sense to use a dedicated state for tracking the validity of the mold_num input. It's value is used in many places like conditional rendering, css styles and so on. It does not make sense to run regex on every place where it's used.
+  // I want to attach the validation logic to onChange on the Input component. So users can get feedback about the validity of the input right away.
+  // But I want to make sure that a request is only made when the input is valld and the select or input element looses focus.
+  // Because currently, sys, mold_num and sn_num are locked immediately after the request is made.
+  // TODO: server 500 error when mold_num is A1 which is a valid input.
+  const [isMoldNumValid, setIsMoldNumValid] = useState<boolean>(false);
+
+  const onBlur = () => {
+    if (!moldNum || !sys || !isMoldNumValid) return;
+    handleGetNewSnNum();
+  };
 
   const handleReset = () => {
     setIsSnNumReady(false);
@@ -88,22 +96,18 @@ export default function CreateMold({
       <Modal isOpen={showModal} onClose={() => setShowModal(false)}>
         <form
           action=""
-          // onSubmit={handleCreateNewMold}
           onSubmit={onSubmit}
           id="Create Mold"
           className="m-auto grid w-fit grid-flow-row grid-cols-2 gap-8 text-nowrap border p-2"
         >
           <label
-            onBlur={() => {
-              if (sys !== "" && moldNum) handleGetNewSnNum();
-            }}
+            onBlur={onBlur}
             className="flex h-full items-center justify-between gap-4 p-2"
-            // htmlFor="sys"
           >
             {/* <label htmlFor="sys"> */}
             <span className="w-1/4">系列</span>
             <Select
-              options={seriesOptions}
+              options={seriesOptions.filter((option) => option.text !== "模仁")}
               className="w-3/4"
               disabled={isSnNumReady}
               // unable to reset.
@@ -117,20 +121,27 @@ export default function CreateMold({
 
           <label
             className="flex h-full items-center justify-between gap-4 p-2"
-            onBlur={() => {
-              if (sys !== "" && moldNum) handleGetNewSnNum();
-            }}
+            onBlur={onBlur}
           >
             <span className="w-1/4">模號</span>
-            <Input
-              type="text"
-              className="w-3/4"
-              readOnly={isSnNumReady}
-              value={moldNum}
-              onChange={(e) => {
-                setMoldNum(e.target.value);
-              }}
-            />
+            <span className="relative w-3/4">
+              <Input
+                type="text"
+                maxLength={2}
+                readOnly={isSnNumReady}
+                value={moldNum}
+                onChange={(e) => {
+                  setMoldNum(e.target.value);
+                  setIsMoldNumValid(regexforMoldNumInput.test(e.target.value));
+                }}
+              />
+              {moldNum && !isMoldNumValid && (
+                <span className="absolute -left-1 top-8 p-2 text-red-500">
+                  {/* mold_num is valid: {isMoldNumValid.toString()} */}
+                  未符合定義格式
+                </span>
+              )}
+            </span>
           </label>
           <label className="flex h-full items-center justify-between gap-4 p-2">
             <span className="w-1/4">系列唯一碼</span>
@@ -141,11 +152,17 @@ export default function CreateMold({
               readOnly
             />
           </label>
-          <CreateMoldSubComponentWithPreData
+          {/* <CreateMoldSubComponentWithPreData
             isSnNumReady={isSnNumReady}
             handleReset={handleReset}
-          />
-          {/* {isSnNumReady && <CreateMoldSubComponentWithPreData isSnNumReady />} */}
+          /> */}
+
+          {isSnNumReady && (
+            <CreateMoldSubComponentWithPreData
+              isSnNumReady={isSnNumReady}
+              handleReset={handleReset}
+            />
+          )}
           {/* <Select name="maker" options={makerOptions} /> */}
         </form>
       </Modal>
