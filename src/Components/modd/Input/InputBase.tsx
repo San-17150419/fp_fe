6129c6cc25d4f0cc303f9458301d1cae3a6550ec
ZENG-JING-YUAN <steven.tseng@heavypower.com.tw>
2024-09-19 14:49:37 +0800

    fix: adjust InputBase component styles
    - Removed redundant text color class and reordered class list for
    better readability.
    - Correct conditional classes for disabled and readOnly states.

--- src/Components/modd/Input/InputBase.tsx
+++ src/Components/modd/Input/InputBase.tsx
@@ -8,12 +8,11 @@ const Input = forwardRef<HTMLInputElement, InputBaseProps>((props, ref) => {
       ref={ref}
       {...props}
       className={cn(
-        "h-9 w-full rounded-md border border-gray-300 px-2 text-base font-semibold text-slate-600 shadow shadow-slate-300 focus:outline-none desktop:px-3",
+        "h-9 w-full rounded-md border border-gray-300 bg-white px-2 text-base font-semibold shadow shadow-slate-300 focus:outline-none desktop:px-3",
         props.className,
-        props.disabled ||
-          (props.readOnly
-            ? "opacity-50 pointer-events-none caret-transparent"
-            : "cursor-pointer hover:border-blue-300 focus:border-blue-500 "),
+        props.disabled || props.readOnly
+          ? "pointer-events-none caret-transparent opacity-50"
+          : "cursor-pointer hover:border-blue-300 focus:border-blue-500",
       )}
     />
   );
