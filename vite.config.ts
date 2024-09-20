53dca8babc1ef7eb29b7ebb37ad606230865718c
ZENG-JING-YUAN <steven.tseng@heavypower.com.tw>
2024-09-20 16:05:20 +0800

    feat: Update Vite configuration
    - Integrated `vite-plugin-mkcert` into the Vite configuration to enable
    local HTTPS support.
    - Configured the plugin to store certificates in the `./certs` directory
    and force certificate generation.
    - Updated the `devServer` configuration to use the generated SSL
    certificates for development (`cert.pem` and `dev.pem`).

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,20 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
-
+import mkcert from "vite-plugin-mkcert";
 // https://vitejs.dev/config/
+
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
   return {
     define: {
       "process.env.SOME_KEY": JSON.stringify(env.SOME_KEY),
     },
-    build: {
-      sourcemap: true,
+    plugins: [react(), mkcert({ savePath: "./certs", force: true })],
+    devServer: {
+      https: {
+        cert: "./certs/cert.pem",
+        key: "./certs/dev.pem",
+      },
     },
-    plugins: [react()],
   };
 });
