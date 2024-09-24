ddbf0dac77276f174e0c3c2eacbc1b12d20b4e5f
ZENG-JING-YUAN <steven.tseng@heavypower.com.tw>
2024-09-24 10:02:09 +0800

    refactor: Enhance `FieldInfo` component
    - Removed most default style in `FieldInfo` component.
    - Updated `FieldInfo` to accept an optional `className` prop,
    allowing for more customizable styling.

--- src/features/EngineerDepartment/component/TanStackForm/FieldInfo.tsx
+++ src/features/EngineerDepartment/component/TanStackForm/FieldInfo.tsx
@@ -1,9 +1,15 @@
 import { FieldApi } from "@tanstack/react-form";
-export default function FieldInfo({ field }: { field: FieldApi<any, any, any, any> }) {
+import cn from "../../../../util/cn";
+
+type FieldInfoProps = {
+  field: FieldApi<any, any, any, any>;
+  className?: string;
+}
+export default function FieldInfo({ field, className }:FieldInfoProps) {
   return (
     <>
       {field.state.meta.isTouched && field.state.meta.errors.length ? (
-        <em className="absolute left-28 top-12 text-red-500">
+        <em className={cn("text-red-500", className)}>
           {field.state.meta.errors.join(", ")}
         </em>
       ) : null}
