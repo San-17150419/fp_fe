981e0ee5b164b5b32201a148b59f4843bd03dacc
ZENG-JING-YUAN <steven.tseng@heavypower.com.tw>
2024-09-24 16:31:36 +0800

    feat: Add FormSelectField component
    - Created a new `FormSelectField` component that integrates `Select`
    with TanStack's form field API.
    - The component allows flexibility by conditionally requiring `value`
    and `onChange` based on whether the `onChange` prop is provided. If
    `onChange` is defined, `value` becomes mandatory.
    - By default, `value` is tied to the form field's state, but it can be
    overridden if custom logic is needed to control the select's value
    externally.
    - `onChange` propagates changes to both the form field state and the
    external handler, if provided.
    
    This addition enhances the form's flexibility by combining TanStack's form management with customizable select field behavior.

---
+++ src/features/EngineerDepartment/component/TanStackForm/FormSelectField.tsx
@@ -0,0 +1,42 @@
+import { type FieldApi } from "@tanstack/react-form";
+import FormField from "./FormField";
+import Select, {
+  type SelectProps as BaseSelectProps,
+  type Option,
+} from "../../../../Components/modd/Select/Select";
+
+export type FormSelectFieldProps<T> = Omit<
+  BaseSelectProps<T>,
+  "value" | "onChange"
+> & {
+  field: FieldApi<any, any, any, any>;
+  value?: BaseSelectProps<T>["value"];
+  onChange?: (option: Option<T>) => void;
+} & (BaseSelectProps<T>["onChange"] extends undefined
+    ? {} // If onChange is not provided, value can be optional
+    : {
+        value: BaseSelectProps<T>["value"];
+        onChange: (option: Option<T>) => void;
+      }); // If onChange is provided, value must be required
+
+export default function FormSelectField<T>({
+  field,
+  ...selectProps
+}: FormSelectFieldProps<T>) {
+  return (
+    <FormField field={field}>
+      <Select
+        {...selectProps}
+        // By default, the `value` of the select is the `value` of the field. But we can override that with the `value` prop if the consumer wants to control the value of the select.(for examlpe, add specific logic to control the value of the select when the field changes)
+
+        // 
+        value={selectProps.value ?? field.state.value}
+        // By default, the `value` is updated internally, so the value of field and select are the same. This cannot be overridden. But if the consumer needs to keep the value in sync with an external state, they can use the `onChange` prop to do that.
+        onChange={(e) => {
+          field.handleChange(e);
+          selectProps.onChange?.(e);
+        }}
+      />
+    </FormField>
+  );
+}
