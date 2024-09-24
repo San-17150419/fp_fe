e8c544a194fe2ae8ecb36cb4abc85c485a0085ff
ZENG-JING-YUAN <steven.tseng@heavypower.com.tw>
2024-09-24 10:02:28 +0800

    refacto: Introduce styled FieldInfo in FormField
    - Changed import name of `FieldInfo` to `FieldInfoBase` to avoid name
    crash and provide more clarity for its purpose.
    - Created a styled `FieldInfo` component within the `FormField`
    component, applying necessary default styles using `FieldInfoBase`.
    - This styled `FieldInfo` allows the `FormField` component to maintain
    its current styling requirements while leveraging the updated,
     more flexible `FieldInfoBase`.

--- src/features/EngineerDepartment/component/TanStackForm/FormField.tsx
+++ src/features/EngineerDepartment/component/TanStackForm/FormField.tsx
@@ -1,27 +1,35 @@
 import { FieldApi } from "@tanstack/react-form";
 import { ReactNode } from "react";
 import { useTranslation } from "react-i18next";
-import FieldInfo from "./FieldInfo";
+import FieldInfoBase from "./FieldInfo";
 
 export default function FormField({
-    field,
-    children,
-  }: {
-    field: FieldApi<any, any, any, any>;
-    children: ReactNode;
-  }) {
-    const { t } = useTranslation();
-    return (
-      <>
-        <label
-          htmlFor={field.name}
-          className="relative flex h-full items-center justify-between gap-6 p-2"
-        >
-          <span className="w-1/4">{t(field.name)}</span>
-          <span className="w-3/4">{children}</span>
-          <FieldInfo field={field} />
-        </label>
-      </>
-    );
-  }
-  
+  field,
+  children,
+}: {
+  field: FieldApi<any, any, any, any>;
+  children: ReactNode;
+}) {
+  const { t } = useTranslation();
+  return (
+    <>
+      <label
+        htmlFor={field.name}
+        className="relative flex h-full items-center justify-between gap-6 p-2"
+      >
+        <span className="w-1/4">{t(field.name)}</span>
+        <span className="w-3/4">{children}</span>
+        <FieldInfo field={field} />
+      </label>
+    </>
+  );
+}
+
+function FieldInfo({ field }: { field: FieldApi<any, any, any, any> }) {
+  return (
+    <FieldInfoBase
+      field={field}
+      className="absolute left-28 top-12 text-red-500"
+    />
+  );
+}
