58b3d6ade08272186d886adea4f868686f8da6a2
ZENG-JING-YUAN <steven.tseng@heavypower.com.tw>
2024-09-25 11:21:13 +0800

    refactor: Enhance FormInputField component
    Enhanced type handling and value management in FormInputField component
    
    **Description**:
    - Updated the `FormInputField` component to handle `value` and
    `onChange` props more flexibly.
    - Introduced `FormInputFieldProps` type using `Omit` to exclude `value`
     and `onChange` from `InputBaseProps`, and provided conditional typing
     for optional vs. mandatory `value` and `onChange`.
    - Adjusted the input rendering logic to prioritize external `value`
     prop if available, otherwise using the field's state value.
    - Improved the change handler to trigger both field updates and any
    external `onChange` logic.

--- src/features/EngineerDepartment/component/TanStackForm/FormInputField.tsx
+++ src/features/EngineerDepartment/component/TanStackForm/FormInputField.tsx
@@ -4,15 +4,34 @@ import Input, {
 } from "../../../../Components/modd/Input/InputBase";
 import { type FieldApi } from "@tanstack/react-form";
 
-type FormFieldProps = InputBaseProps & { field: FieldApi<any, any, any, any> };
-
+export type FormInputFieldProps = Omit<InputBaseProps, "value" | "onChange"> & {
+  field: FieldApi<any, any, any, any>;
+} & (
+    | {
+        value?: InputBaseProps["value"];
+        onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+      }
+    | {
+        value: InputBaseProps["value"];
+        onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+      }
+  );
 export default function FormInputFiled({
   field,
   ...inputProps
-}: FormFieldProps) {
+}: FormInputFieldProps) {
   return (
     <FormField field={field}>
-      <Input {...inputProps} />
+      <Input
+        {...inputProps}
+        name={field.name}
+        onChange={(e) => {
+          field.handleChange(e.target.value);
+          inputProps.onChange && inputProps.onChange(e);
+        }}
+        // the input value prioritizes the external `value` prop if available. Otherwise, the value of the field is used.
+        value={inputProps.value && !inputProps.defaultValue ? inputProps.value : field.state.value}
+      />
     </FormField>
   );
 }
