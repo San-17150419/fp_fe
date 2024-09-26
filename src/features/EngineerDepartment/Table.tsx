a6ba42ba013d443ad14bf26a5e509812e54a88b7
ZENG-JING-YUAN <steven.tseng@heavypower.com.tw>
2024-09-26 16:56:21 +0800

    feat: begin virtualization of Table component
    - Attempted the virtualization of the `Table` component by integrating
    `react-render-if-visible` to only render rows that are visible in the
    viewport.
    - Removed the direct use of `isLoading` .
    
    Still facing the same problem with other virtualization libraries.

--- src/features/EngineerDepartment/Table.tsx
+++ src/features/EngineerDepartment/Table.tsx
@@ -1,10 +1,10 @@
-import { useState, useMemo, memo, useCallback } from "react";
+import { useState, useMemo, memo, useCallback, useRef } from "react";
 import clsx from "clsx";
 import { useTranslation } from "react-i18next";
 import { type FilterData } from "./types";
 import FilterForTable from "./FilterForTable";
 import TableRow from "./TableRow";
-import Loading from "../../Components/Loading";
+import RenderIfVisible from "react-render-if-visible";
 
 // TODO: Vitualized table. This component with 600 plus rows contributed over 15000 DOM Nodes.
 
@@ -12,12 +12,10 @@ type TableProps = {
   data: FilterData["data"];
   // data: { [key: string]: number | string | undefined }[];
   //  header props act as a key to the data. Allow you to decide the order of the table and what to show. If the header props is an object, the key of the object will be used as key of the data and the value of the object will be used as the header
-  isLoading: boolean;
   visibleList: string[];
 };
 const memoizedTable = memo(function Table({
   data,
-  isLoading,
   visibleList,
 }: TableProps) {
   const header: Record<string, string>[] = [
@@ -39,7 +37,7 @@ const memoizedTable = memo(function Table({
     { spare: "備註" },
     { id_ms: "資料表id" },
   ];
-
+  const tableRef = useRef<HTMLTableElement>(null);
   const [visibleColumns, setVisibleColumns] = useState({
     sn_num: true,
     sys: true,
@@ -77,21 +75,31 @@ const memoizedTable = memo(function Table({
     const tableRows: Record<string, JSX.Element> = {};
     data.forEach((row) => {
       tableRows[row.sn_num] = (
-        <TableRow data={row} key={row.id_ms} visibleColumns={visibleColumns} />
+        <tr>
+          <TableRow
+            data={row}
+            key={row.id_ms}
+            visibleColumns={visibleColumns}
+          />
+        </tr>
       );
     });
     return tableRows;
   }, [data, visibleColumns]);
   return (
     <>
-      {isLoading && <Loading />}
+      {/* {isLoading && <Loading  text="Table loading"/>} */}
       <FilterForTable
         visibleColumns={visibleColumns}
         onColumnToggle={handleColumnToggle}
         header={header}
       />
-      <div className="h-[600px] max-w-full overflow-auto">
-        <table className="relative w-full table-auto border-separate border-spacing-0 text-nowrap text-center">
+      <div className="h-[60vh] max-w-full overflow-auto">
+        <table
+          className="relative w-full table-auto border-separate border-spacing-0 text-nowrap text-center"
+          style={{ willChange: "auto" }}
+          ref={tableRef}
+        >
           <thead className="sticky top-0 bg-gray-300">
             <tr>
               {memoizedColumnsOrder.map((key) => (
@@ -101,6 +109,7 @@ const memoizedTable = memo(function Table({
                     visibleColumns[key as keyof typeof visibleColumns]
                       ? ""
                       : "hidden",
+                    // getClassName(key),
                   )}
                   key={`header-${key}`}
                 >
@@ -109,7 +118,7 @@ const memoizedTable = memo(function Table({
               ))}
             </tr>
           </thead>
-          <tbody>
+          {/* <tbody>
             {visibleList?.length > 0 ? (
               visibleList.map(
                 (key) =>
@@ -120,6 +129,23 @@ const memoizedTable = memo(function Table({
                 <td colSpan={memoizedColumnsOrder.length}>{t("No Data")}</td>
               </tr>
             )}
+          </tbody> */}
+          <tbody>
+            {data.map((row) => (
+              <RenderIfVisible
+                key={row.id_ms}
+                defaultHeight={300}
+                root={tableRef.current}
+                rootElement="tr"
+                placeholderElement="td"
+              >
+                <TableRow
+                  data={row}
+                  key={row.id_ms}
+                  visibleColumns={visibleColumns}
+                />
+              </RenderIfVisible>
+            ))}
           </tbody>
         </table>
       </div>
@@ -128,3 +154,44 @@ const memoizedTable = memo(function Table({
 });
 
 export default memoizedTable;
+
+// function getClassName(key: string) {
+//   switch (key) {
+//     case "sn_num":
+//       return "text-blue-400 w-[135px]";
+//     case "sys":
+//       return " w-[90px]";
+//     case "property":
+//       return " w-[85px]";
+//     case "site":
+//       return " w-[60px]";
+//     case "brand":
+//       return "w-[60px]";
+//     case "prod_name_board":
+//       return "text-red-400 w-[215px]";
+//     case "pnb_state":
+//       return "w-[90px]";
+//     case "prod_name_nocolor":
+//       return " w-[160px]";
+//     case "mold_num":
+//       return " w-[70px]";
+//     case "hole_num":
+//       return " w-[70px]";
+//     case "block_num":
+//       return " w-[60px]";
+//     case "dutydate_month":
+//       return "text-sm w-[130px]";
+//     case "dutydate_last":
+//       return "text-sm text-blue-400 w-[130px]";
+//     case "maker":
+//       return "text-sm w-[100px]";
+//     case "state":
+//       return "text-sm w-[100px]";
+//     case "spare":
+//       return "text-sm w-[100px]";
+//     case "id_ms":
+//       return "text-sm w-[100px]";
+//     default:
+//       return "";
+//   }
+// }
