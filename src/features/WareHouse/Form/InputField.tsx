50a2b777a7559dc895c4c77e6c2bc3210f4f8643
ZENG-JING-YUAN <steven.tseng@heavypower.com.tw>
2024-10-01 17:18:58 +0800

    feat: add isRequired prop to InputField component
    - Added the `isRequired` prop to the `InputField` component, allowing
    it to pass the required field indication to the `FormFieldContent`.

--- src/features/WareHouse/Form/InputField.tsx
+++ src/features/WareHouse/Form/InputField.tsx
@@ -6,16 +6,18 @@ import clsx from "clsx";
 type FormInputFieldProps = {
   field: FieldApi<any, any, any, any>;
   span: number;
+  isRequired?: boolean;
 } & InputHTMLAttributes<HTMLInputElement>;
 export default function FormInputField({
   field,
   span,
+  isRequired = false,
   ...inputProps
 }: FormInputFieldProps) {
   return (
     // TODO: It is not connected to the form correctly. For example, the name is not restricted to the default values in the form. I can pass any name to the input field. And it won't give an error.
 
-    <FormFieldContent field={field} span={span}>
+    <FormFieldContent field={field} span={span} isRequired={isRequired}>
       <input
         title={field.name}
         id={field.name}
