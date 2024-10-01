e517e426ea3e84a92daca5d19f793290622bfe40
ZENG-JING-YUAN <steven.tseng@heavypower.com.tw>
2024-10-01 16:24:43 +0800

    refactor: simplify FormInputField component
    - Refactored `FormInputField` to remove unnecessary `useContext` and
    direct dependency on `FormContext`.
    - Replaced `DeepKeys<T>` with `FieldApi`.
    - Updated the input field to use `clsx` for dynamic class handling.
    - Removed redundant form context checks and updated the component to
    directly manage the `field` prop.

--- src/features/WareHouse/Form/InputField.tsx
+++ src/features/WareHouse/Form/InputField.tsx
@@ -1,39 +1,29 @@
-import { FormContext } from "./Form";
-import { useContext, type InputHTMLAttributes } from "react";
-import { type DeepKeys } from "@tanstack/react-form";
+import { type InputHTMLAttributes } from "react";
+import { type FieldApi } from "@tanstack/react-form";
 import FormFieldContent from "./FormField";
+import clsx from "clsx";
 
-type FormInputFieldProps<T> = {
-  fieldName: DeepKeys<T>;
+type FormInputFieldProps = {
+  field: FieldApi<any, any, any, any>;
   span: number;
 } & InputHTMLAttributes<HTMLInputElement>;
-export default function FormInputField<T>({
-  fieldName,
+export default function FormInputField({
+  field,
   span,
   ...inputProps
-}: FormInputFieldProps<T>) {
-  const form = useContext(FormContext);
-
-  if (!form) {
-    throw new Error("FormField must be used within a Form component");
-  }
-
+}: FormInputFieldProps) {
   return (
     // TODO: It is not connected to the form correctly. For example, the name is not restricted to the default values in the form. I can pass any name to the input field. And it won't give an error.
-    <form.Field
-      name={fieldName.toString()}
-      children={(field) => (
-        <FormFieldContent field={field} span={span}>
-          <input
-            title={fieldName.toString()}
-            id={fieldName.toString()}
-            value={field.state.value}
-            onChange={(e) => field.handleChange(e.target.value)}
-            {...inputProps}
-            className="outline"
-          />
-        </FormFieldContent>
-      )}
-    />
+
+    <FormFieldContent field={field} span={span}>
+      <input
+        title={field.name}
+        id={field.name}
+        value={field.state.value}
+        onChange={(e) => field.handleChange(e.target.value)}
+        {...inputProps}
+        className={clsx("w-full outline-none", inputProps.className)}
+      />
+    </FormFieldContent>
   );
 }
