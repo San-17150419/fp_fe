6a712107b6e4b38ea25cb215af6d165470fcd2c1
ZENG-JING-YUAN <steven.tseng@heavypower.com.tw>
2024-10-03 15:24:22 +0800

    feat: enhance OrderDetails component
    - Updated `Tab` styling: Tabs now have rounded corners, hover effects,
    and selected state styling for better user experience.
    - Added a detailed table for inspection items under the "檢驗細項" tab,
    displaying fields like sample size, severity levels, and results.
    - Rendered mock data in the inspection details table to simulate the
    display of actual inspection records.

--- src/features/WareHouse/OrderDetails.tsx
+++ src/features/WareHouse/OrderDetails.tsx
@@ -18,8 +18,12 @@ export default function OrderDetails({ data }: OrderDetailsProps) {
   return (
     <TabGroup className="min-w-[300px] items-center justify-center">
       <TabList className="flex justify-evenly gap-4 border-b-2">
-        <Tab>驗收單細節</Tab>
-        <Tab>檢驗細項</Tab>
+        <Tab className="m-1 rounded-md px-2 py-1 data-[selected]:bg-blue-500 data-[selected]:text-white data-[hover]:underline">
+          驗收單細節
+        </Tab>
+        <Tab className="m-1 rounded-md px-2 py-1 data-[selected]:bg-blue-500 data-[selected]:text-white data-[hover]:underline">
+          檢驗細項
+        </Tab>
       </TabList>
       <TabPanels>
         <TabPanel className="my-4 flex flex-col items-center gap-1">
@@ -60,7 +64,49 @@ export default function OrderDetails({ data }: OrderDetailsProps) {
               />
             ))}
         </TabPanel>
-        <TabPanel>檢驗項目</TabPanel>
+        <TabPanel>
+          檢驗項目
+          <h1 className="outline">
+            {data.doc_class}| {data.order_prodModel}| {data.order_prodNum}
+          </h1>
+          <table className="min-w-[700px] table-fixed text-center">
+            <thead>
+              <tr>
+                <th rowSpan={2}>檢驗項目</th>
+                <th rowSpan={2}>抽樣數</th>
+                <th colSpan={3} className="border-b">
+                  (AC/不良數)
+                </th>
+                <th rowSpan={2}>檢驗記錄</th>
+                <th rowSpan={2}>結果</th>
+              </tr>
+              <tr className="border-b">
+                <th>嚴重</th>
+                <th>主要</th>
+                <th>次要</th>
+              </tr>
+            </thead>
+            <tbody>
+              {Array.from({ length: 10 }).map((_, index) => (
+                <tr key={index} className="border-b even:bg-slate-100">
+                  <td className="w-[80px]">A</td>
+                  <td className="w-[80px]">0</td>
+                  <td className="w-[50px]"></td>
+                  <td className="w-[50px]"></td>
+                  <td className="w-[50px]"></td>
+                  <td className="flex justify-center gap-3 text-sm">
+                    <div className="m-1 px-2 py-1">0.30</div>
+                    <div className="m-1 px-2 py-1">0.28</div>
+                    <div className="m-1 px-2 py-1">0.32</div>
+                    <div className="m-1 px-2 py-1">0.30</div>
+                    <div className="m-1 px-2 py-1">0.30</div>
+                  </td>
+                  <td className="w-[70px]">OK</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </TabPanel>
       </TabPanels>
     </TabGroup>
   );
