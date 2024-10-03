27b8fc6f03c0991d7e0cfddf5e2a94b46522ac69
ZENG-JING-YUAN <steven.tseng@heavypower.com.tw>
2024-10-03 09:58:12 +0800

    fix: Improved sidebar behavior
    - Changed the event listener in `Sidebar.tsx` from "mousedown" to
    "click" to ensure proper behavior for the sidebar when closing. This
    ensure the closing of the sidebar won't interfere with navigation.
    
    Before the changes, clicking link outside of sidebar will only close the
    sidebar. The navigation is "eaten".

--- src/Components/modd/Sidebar/Sidebar.tsx
+++ src/Components/modd/Sidebar/Sidebar.tsx
@@ -22,9 +22,9 @@ export default function Sidebar() {
       }
     }
 
-    document.addEventListener("mousedown", handleClickOutside);
+    document.addEventListener("click", handleClickOutside);
     return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
+      document.removeEventListener("click", handleClickOutside);
     };
   }, [sidebarRef]);
 
