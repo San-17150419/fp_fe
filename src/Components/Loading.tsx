36d03eff3124954278d078d77cf12885af7843b4
ZENG-JING-YUAN <steven.tseng@heavypower.com.tw>
2024-10-04 17:03:52 +0800

    feat: add fullScreen prop to Loading component
    - Introduced a `fullScreen` prop to the `Loading` component, allowing
     it to be used in both full-screen and non-full-screen modes.
    - The `fullScreen` prop defaults to `true` for backward compatibility
    but can be set to `false` for more flexible positioning within a
    container.
    - Adjusted the `div` class to conditionally apply styles based on the
    `fullScreen` flag, supporting both fixed and centered absolute
    positioning.

--- src/Components/Loading.tsx
+++ src/Components/Loading.tsx
@@ -3,11 +3,19 @@ import Loader from "./Loader";
 type LoadingProps = {
   children?: React.ReactNode;
   text?: string;
+  fullScreen?: boolean;
 };
-export default function Loading({ children, text }: LoadingProps) {
+export default function Loading({
+  children,
+  text,
+  fullScreen = true,
+}: LoadingProps) {
   const { t } = useTranslation();
   return (
-    <div className="fixed inset-0 z-50 flex h-full flex-col items-center justify-center gap-5 bg-gray-200/50 text-5xl backdrop-blur-sm">
+    <div
+      className={`z-50 flex h-full w-full flex-col items-center justify-center gap-5 bg-gray-200/50 text-5xl ${fullScreen ? "fixed inset-0 backdrop-blur-sm" : "absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2"}`}
+    >
+      {/* <div className="fixed inset-0 z-50 flex h-full flex-col items-center justify-center gap-5 bg-gray-200/50 text-5xl backdrop-blur-sm"> */}
       {text && <p className="loading font-bold">{t(text)}</p>}
       {children}
       <Loader />
