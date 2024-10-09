33ce4411bb84cd6bf98b10ebca70ffdaa4645756
ZENG-JING-YUAN <steven.tseng@heavypower.com.tw>
2024-10-09 09:27:08 +0800

    feat: add types for returning raw materials
    - Created a new TypeScript file `ReturnRawMaterialTypes.ts` to define
    the types related to returning raw materials.
    - Added `ReturnRawMaterialParams` type to specify parameters for return.
    - Added `ReturnRawMaterialResponse` type to define the structure of the
    response, including fields such as `order` details, `history`, and
    `product_option`.

---
+++ src/features/WareHouse/types/ReturnRawMaterialTypes.ts
@@ -0,0 +1,34 @@
+export type ReturnRawMaterialParams = {
+  dorder_num: string;
+};
+
+export type ReturnRawMaterialResponse = {
+  post: ReturnRawMaterialParams;
+  order: {
+    id_dorder: number;
+    date: string;
+    dorder_num: string;
+    supplier_code: string;
+    process: string;
+    do_prodModel: string;
+    do_product: string;
+    amt_order: number;
+    amt_unit: string;
+    do_prodNum: string;
+    item_size: string;
+    product_no: string;
+    history: {
+      id_dorder: number;
+      total_sent: number | null;
+      total_back: number | null;
+    } | null;
+    product_option: {
+      item_name: string;
+      item_code: string;
+    }[];
+  }[];
+  info_check: {
+    status: string;
+    message: string;
+  };
+};
